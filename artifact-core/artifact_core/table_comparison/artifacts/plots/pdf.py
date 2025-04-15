811496bea0c6f0ac0412ba7fa76c357d6913b354
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-04-15 12:17:43 +0300

    Implement pdf comparison plot artifacts

---
+++ artifact-core/artifact_core/table_comparison/artifacts/plots/pdf.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from matplotlib.figure import Figure
+
+from artifact_core.base.artifact_dependencies import NoArtifactHyperparams
+from artifact_core.libs.implementation.pdf.overlaid_plotter import (
+    OverlaidPDFPlotter,
+)
+from artifact_core.table_comparison.artifacts.base import (
+    TableComparisonPlot,
+)
+class PDFComparisonCombinedPlot(TableComparisonPlot[NoArtifactHyperparams]):
+    def _compare_datasets(
+        self, dataset_real: pd.DataFrame, dataset_synthetic: pd.DataFrame
+    ) -> Figure:
+        dict_plots = OverlaidPDFPlotter.get_overlaid_pdf_plot(
+            dataset_real=dataset_real,
+            dataset_synthetic=dataset_synthetic,
+            ls_features_order=self._data_spec.ls_features,
+            ls_cts_features=self._data_spec.ls_cts_features,
+            ls_cat_features=self._data_spec.ls_cat_features,
+            cat_unique_map=self._data_spec.categorical_unique_map,
+        )
+        return dict_plots
