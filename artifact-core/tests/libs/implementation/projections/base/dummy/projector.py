fdc59a1d97020aef2187c08da6729a6c3b86ba28
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-04-17 17:33:44 +0300

    Add dummy projector for testing

---
+++ artifact-core/tests/libs/implementation/projections/base/dummy/projector.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from typing import List, Optional, Type, TypeVar
+
+import numpy as np
+import pandas as pd
+from artifact_core.libs.implementation.projections.base.plotter import (
+    ProjectionPlotter,
+    ProjectionPlotterConfig,
+)
+from artifact_core.libs.implementation.projections.base.projector import (
+    ProjectorBase,
+    ProjectorHyperparams,
+)
+
+dummyProjectorT = TypeVar("dummyProjectorT", bound="DummyProjector")
+
+
+@dataclass(frozen=True)
+class DummyProjectorHyperparams(ProjectorHyperparams):
+    projection_type: str = "random"
+
+
+class DummyProjector(ProjectorBase[DummyProjectorHyperparams]):
+    @classmethod
+    def build(
+        cls: Type[dummyProjectorT],
+        ls_cat_features: List[str],
+        ls_cts_features: List[str],
+        projector_config: Optional[DummyProjectorHyperparams] = None,
+        plotter_config: Optional[ProjectionPlotterConfig] = None,
+    ) -> dummyProjectorT:
+        if projector_config is None:
+            projector_config = DummyProjectorHyperparams()
+        if plotter_config is None:
+            plotter_config = ProjectionPlotterConfig()
+        plotter = ProjectionPlotter(config=plotter_config)
+        return cls(
+            ls_cat_features=ls_cat_features,
+            ls_cts_features=ls_cts_features,
+            hyperparams=projector_config,
+            plotter=plotter,
+        )
+
+    @classmethod
+    def _get_projection_name(cls) -> str:
+        return "dummy_projection"
+
+    def _project(self, dataset_preprocessed: pd.DataFrame) -> Optional[np.ndarray]:
+        if self._hyperparams.projection_type == "random":
+            n_samples = len(dataset_preprocessed)
+            n_cols = len(dataset_preprocessed.columns)
+            return np.random.rand(n_samples, n_cols)
+        else:
+            raise ValueError(f"Unknown projection type: {self._hyperparams.projection_type}")
