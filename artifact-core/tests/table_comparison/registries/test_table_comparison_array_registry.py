9537981a150c7e3af6d95508300e836d6aabbdff
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-04-19 18:08:22 +0300

    Test table comparison registries

---
+++ artifact-core/tests/table_comparison/registries/test_table_comparison_array_registry.py
@@ -0,0 +1,24 @@
+from typing import Type
+
+import pytest
+from artifact_core.libs.data_spec.tabular.protocol import TabularDataSpecProtocol
+from artifact_core.table_comparison.artifacts.base import (
+    TableComparisonArray,
+)
+from artifact_core.table_comparison.registries.arrays.registry import (
+    TableComparisonArrayRegistry,
+    TableComparisonArrayType,
+)
+
+
+@pytest.mark.parametrize(
+    "artifact_type, artifact_class",
+    [],
+)
+def test_get(
+    data_spec: TabularDataSpecProtocol,
+    artifact_type: TableComparisonArrayType,
+    artifact_class: Type[TableComparisonArray],
+):
+    artifact = TableComparisonArrayRegistry.get(artifact_type=artifact_type, data_spec=data_spec)
+    assert isinstance(artifact, artifact_class)
---
+++ artifact-core/tests/table_comparison/registries/test_table_comparison_plot_registry.py
@@ -0,0 +1,80 @@
+from typing import Type
+
+import pytest
+from artifact_core.libs.data_spec.tabular.protocol import TabularDataSpecProtocol
+from artifact_core.table_comparison.artifacts.base import (
+    TableComparisonPlot,
+)
+from artifact_core.table_comparison.artifacts.plots.cdf import CDFComparisonCombinedPlot
+from artifact_core.table_comparison.artifacts.plots.descriptive_stat_comparison import (
+    ContinuousFeatureDescriptiveStatsComparisonPlot,
+    ContinuousFeatureFirstQuartileComparisonPlot,
+    ContinuousFeatureMaximaComparisonPlot,
+    ContinuousFeatureMeanComparisonPlot,
+    ContinuousFeatureMedianComparisonPlot,
+    ContinuousFeatureMinimaComparisonPlot,
+    ContinuousFeatureSTDComparisonPlot,
+    ContinuousFeatureThirdQuartileComparisonPlot,
+    ContinuousFeatureVarianceComparisonPlot,
+)
+from artifact_core.table_comparison.artifacts.plots.pairwise_correlations import (
+    CorrelationComparisonCombinedPlot,
+)
+from artifact_core.table_comparison.artifacts.plots.pca_projection import (
+    PCAProjectionComparisonPlot,
+)
+from artifact_core.table_comparison.artifacts.plots.pdf import PDFComparisonCombinedPlot
+from artifact_core.table_comparison.artifacts.plots.truncated_svd_projection import (
+    TruncatedSVDProjectionComparisonPlot,
+)
+from artifact_core.table_comparison.artifacts.plots.tsne_projection import (
+    TSNEProjectionComparisonPlot,
+)
+from artifact_core.table_comparison.registries.plots.registry import (
+    TableComparisonPlotRegistry,
+    TableComparisonPlotType,
+)
+
+
+@pytest.mark.parametrize(
+    "artifact_type, artifact_class",
+    [
+        (TableComparisonPlotType.PDF_PLOT, PDFComparisonCombinedPlot),
+        (TableComparisonPlotType.CDF_PLOT, CDFComparisonCombinedPlot),
+        (
+            TableComparisonPlotType.DESCRIPTIVE_STATS_COMPARISON_PLOT,
+            ContinuousFeatureDescriptiveStatsComparisonPlot,
+        ),
+        (TableComparisonPlotType.MEAN_COMPARISON_PLOT, ContinuousFeatureMeanComparisonPlot),
+        (TableComparisonPlotType.STD_COMPARISON_PLOT, ContinuousFeatureSTDComparisonPlot),
+        (TableComparisonPlotType.VARIANCE_COMPARISON_PLOT, ContinuousFeatureVarianceComparisonPlot),
+        (TableComparisonPlotType.MEDIAN_COMPARISON_PLOT, ContinuousFeatureMedianComparisonPlot),
+        (
+            TableComparisonPlotType.FIRST_QUARTILE_COMPARISON_PLOT,
+            ContinuousFeatureFirstQuartileComparisonPlot,
+        ),
+        (
+            TableComparisonPlotType.THIRD_QUARTILE_COMPARISON_PLOT,
+            ContinuousFeatureThirdQuartileComparisonPlot,
+        ),
+        (TableComparisonPlotType.MIN_COMPARISON_PLOT, ContinuousFeatureMinimaComparisonPlot),
+        (TableComparisonPlotType.MAX_COMPARISON_PLOT, ContinuousFeatureMaximaComparisonPlot),
+        (
+            TableComparisonPlotType.PAIRWISE_CORRELATION_COMPARISON_HEATMAP,
+            CorrelationComparisonCombinedPlot,
+        ),
+        (TableComparisonPlotType.PCA_PROJECTION_PLOT, PCAProjectionComparisonPlot),
+        (
+            TableComparisonPlotType.TRUNCATED_SVD_PROJECTION_PLOT,
+            TruncatedSVDProjectionComparisonPlot,
+        ),
+        (TableComparisonPlotType.TSNE_PROJECTION_PLOT, TSNEProjectionComparisonPlot),
+    ],
+)
+def test_get(
+    data_spec: TabularDataSpecProtocol,
+    artifact_type: TableComparisonPlotType,
+    artifact_class: Type[TableComparisonPlot],
+):
+    artifact = TableComparisonPlotRegistry.get(artifact_type=artifact_type, data_spec=data_spec)
+    assert isinstance(artifact, artifact_class)
