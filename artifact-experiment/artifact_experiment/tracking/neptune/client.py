478c89be48021a90daf33a83224b5b971bd9ae91
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-04-27 16:23:20 +0300

    Implement Neptune artifact tracking client

---
+++ artifact-experiment/artifact_experiment/tracking/neptune/client.py
@@ -0,0 +1,71 @@
+from typing import Dict, Optional, Type
+
+import neptune
+from matplotlib.figure import Figure
+from numpy import ndarray
+
+from artifact_experiment.base.tracking.client import TrackingClient
+from artifact_experiment.base.tracking.logger import ArtifactLogger
+from artifact_experiment.tracking.neptune.backend import NeptuneBackend
+from artifact_experiment.tracking.neptune.loggers.array_collections import (
+    NeptuneArrayCollectionLogger,
+)
+from artifact_experiment.tracking.neptune.loggers.arrays import NeptuneArrayLogger
+from artifact_experiment.tracking.neptune.loggers.plot_collections import (
+    NeptunePlotCollectionLogger,
+)
+from artifact_experiment.tracking.neptune.loggers.plots import NeptunePlotLogger
+from artifact_experiment.tracking.neptune.loggers.score_collections import (
+    NeptuneScoreCollectionLogger,
+)
+from artifact_experiment.tracking.neptune.loggers.scores import NeptuneScoreLogger
+
+
+class NeptuneTrackingClient(TrackingClient[NeptuneBackend]):
+    @classmethod
+    def build(
+        cls: Type["NeptuneTrackingClient"], experiment_id: Optional[str] = None
+    ) -> "NeptuneTrackingClient":
+        backend = NeptuneBackend.build(experiment_id=experiment_id)
+        client = NeptuneTrackingClient(backend=backend)
+        return client
+
+    @classmethod
+    def from_native_client(
+        cls: Type["NeptuneTrackingClient"], native_client: neptune.Run
+    ) -> "NeptuneTrackingClient":
+        backend = NeptuneBackend.from_native_client(native_client=native_client)
+        client = NeptuneTrackingClient(backend=backend)
+        return client
+
+    @staticmethod
+    def _get_score_logger(backend: NeptuneBackend) -> ArtifactLogger[float, NeptuneBackend]:
+        return NeptuneScoreLogger(backend=backend)
+
+    @staticmethod
+    def _get_array_logger(
+        backend: NeptuneBackend,
+    ) -> ArtifactLogger[ndarray, NeptuneBackend]:
+        return NeptuneArrayLogger(backend=backend)
+
+    @staticmethod
+    def _get_plot_logger(backend: NeptuneBackend) -> ArtifactLogger[Figure, NeptuneBackend]:
+        return NeptunePlotLogger(backend=backend)
+
+    @staticmethod
+    def _get_score_collection_logger(
+        backend: NeptuneBackend,
+    ) -> ArtifactLogger[Dict[str, float], NeptuneBackend]:
+        return NeptuneScoreCollectionLogger(backend=backend)
+
+    @staticmethod
+    def _get_array_collection_logger(
+        backend: NeptuneBackend,
+    ) -> ArtifactLogger[Dict[str, ndarray], NeptuneBackend]:
+        return NeptuneArrayCollectionLogger(backend=backend)
+
+    @staticmethod
+    def _get_plot_collection_logger(
+        backend: NeptuneBackend,
+    ) -> ArtifactLogger[Dict[str, Figure], NeptuneBackend]:
+        return NeptunePlotCollectionLogger(backend=backend)
