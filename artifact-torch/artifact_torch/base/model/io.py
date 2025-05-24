af9ec0b630db9f58456e8b8aab4bfde1c1122c8d
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-05-24 17:40:07 +0300

    Define model io types

---
+++ artifact-torch/artifact_torch/base/model/io.py
@@ -0,0 +1,27 @@
+from typing import Optional, TypedDict
+
+import torch
+
+
+class ModelIO(TypedDict):
+    pass
+
+
+class ModelInput(ModelIO):
+    pass
+
+
+class SingleTensorInput(ModelInput):
+    t_in: torch.Tensor
+
+
+class LabeledTensorInput(SingleTensorInput):
+    t_labels: Optional[torch.Tensor]
+
+
+class ModelOutput(ModelIO):
+    pass
+
+
+class LossOutput(ModelOutput):
+    t_loss: Optional[torch.Tensor]
