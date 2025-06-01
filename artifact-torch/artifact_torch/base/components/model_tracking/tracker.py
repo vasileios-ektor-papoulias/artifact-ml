d3bfe3301d8d70a40043166b3fa4067bb56b0ae5
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-06-01 12:34:24 +0300

    Restructure model tracking logic

--- artifact-torch/artifact_torch/base/components/model_tracking/tracker.py
+++ artifact-torch/artifact_torch/base/components/model_tracking/tracker.py
@@ -1,52 +1,55 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict
-
-import numpy as np
+from dataclasses import dataclass
+from typing import Any, Dict, Generic, TypeVar
 
 from artifact_torch.base.model.base import Model
 
+ModelTrackingCriterionT = TypeVar("ModelTrackingCriterionT", bound="ModelTrackingCriterion")
 
-class ModelTracker(ABC):
-    def __init__(self, score_key: str):
-        self._score_key = score_key
+
+@dataclass
+class ModelTrackingCriterion: ...
+
+
+@dataclass
+class NoTrackingCriterion(ModelTrackingCriterion): ...
+
+
+class ModelTracker(ABC, Generic[ModelTrackingCriterionT]):
+    def __init__(self):
         self.reset()
-
-    @property
-    def score_key(self) -> str:
-        return self._score_key
 
     @property
     def best_model_state(self) -> Dict[str, Any]:
         return self._best_model_state
 
     @property
-    def best_score(self) -> float:
-        return self._best_score
+    def best_criterion(self) -> ModelTrackingCriterionT:
+        return self._best_criterion
 
     @property
     def best_epoch(self) -> int:
         return self._best_epoch
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _new_score_is_best(new_score: float, best_score: float) -> bool: ...
+    def _new_criterion_is_best(
+        cls, new_criterion: ModelTrackingCriterionT, best_criterion: ModelTrackingCriterionT
+    ) -> bool: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_initial_best_score() -> float: ...
+    def _get_initial_best_criterion(cls) -> ModelTrackingCriterionT: ...
 
-    def update(self, model: Model[Any, Any], score: float, epoch: int):
-        if self.__new_score_is_best(new_score=score):
-            self._best_score = score
+    def update(self, model: Model[Any, Any], criterion: ModelTrackingCriterionT, epoch: int):
+        if self._new_criterion_is_best(
+            new_criterion=criterion, best_criterion=self._best_criterion
+        ):
+            self._best_score = criterion
             self._best_model_state = model.state_dict()
             self._best_epoch = epoch
 
     def reset(self):
         self._best_model_state = {}
-        self._best_score = self._get_initial_best_score()
+        self._best_criterion = self._get_initial_best_criterion()
         self._best_epoch = 0
-
-    def __new_score_is_best(self, new_score: float) -> bool:
-        if np.isnan(self._best_score):
-            return True
-        return self._new_score_is_best(new_score=new_score, best_score=self._best_score)
