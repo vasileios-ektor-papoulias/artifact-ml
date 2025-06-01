3eaf1f7b67612daec4963db35acda3d39bb53855
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-06-01 12:36:33 +0300

    Implement basic stoppers

---
+++ artifact-torch/artifact_torch/libs/components/early_stopping/epoch_bound.py
@@ -0,0 +1,12 @@
+from artifact_torch.base.components.early_stopping.stopper import (
+    EarlyStopper,
+    StopperUpdateData,
+)
+
+
+class EpochBoundStopper(EarlyStopper[StopperUpdateData]):
+    def __init__(self, max_n_epochs: int):
+        super().__init__(max_n_epochs=max_n_epochs)
+
+    def stopping_condition_met(self) -> bool:
+        return self._max_epochs_exceeded()
