7ee75a62e7978f068ba4621f97c60e7273e13238
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-06-03 20:16:49 +0300

    Build validation routine for demo

---
+++ artifact-torch/demo/trainer/validation_plan.py
@@ -0,0 +1,56 @@
+from typing import List
+
+from artifact_experiment.table_comparison.validation_plan import (
+    TableComparisonArrayCollectionType,
+    TableComparisonArrayType,
+    TableComparisonPlotCollectionType,
+    TableComparisonPlotType,
+    TableComparisonScoreCollectionType,
+    TableComparisonScoreType,
+    TableComparisonValidationPlan,
+)
+
+
+class TabularVAEValidationPlan(TableComparisonValidationPlan):
+    @staticmethod
+    def _get_score_types() -> List[TableComparisonScoreType]:
+        return [
+            TableComparisonScoreType.MEAN_JS_DISTANCE,
+            TableComparisonScoreType.PAIRWISE_CORRELATION_DISTANCE,
+        ]
+
+    @staticmethod
+    def _get_array_types() -> List[TableComparisonArrayType]:
+        return []
+
+    @staticmethod
+    def _get_plot_types() -> List[TableComparisonPlotType]:
+        return [
+            TableComparisonPlotType.PDF_PLOT,
+            TableComparisonPlotType.CDF_PLOT,
+            TableComparisonPlotType.DESCRIPTIVE_STATS_COMPARISON_PLOT,
+            TableComparisonPlotType.PCA_PROJECTION_PLOT,
+            TableComparisonPlotType.PAIRWISE_CORRELATION_COMPARISON_HEATMAP,
+        ]
+
+    @staticmethod
+    def _get_score_collection_types() -> List[TableComparisonScoreCollectionType]:
+        return [
+            TableComparisonScoreCollectionType.JS_DISTANCE,
+        ]
+
+    @staticmethod
+    def _get_array_collection_types() -> List[TableComparisonArrayCollectionType]:
+        return [
+            TableComparisonArrayCollectionType.MEANS,
+            TableComparisonArrayCollectionType.STDS,
+            TableComparisonArrayCollectionType.MINIMA,
+            TableComparisonArrayCollectionType.MAXIMA,
+        ]
+
+    @staticmethod
+    def _get_plot_collection_types() -> List[TableComparisonPlotCollectionType]:
+        return [
+            TableComparisonPlotCollectionType.PDF_PLOTS,
+            TableComparisonPlotCollectionType.CDF_PLOTS,
+        ]
