bd4300a8ca6e44184a2ef55a94c0749690782189
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-06-05 17:36:23 +0300

    implement checkpoint logic

--- artifact-torch/artifact_torch/base/components/callbacks/checkpoint.py
+++ artifact-torch/artifact_torch/base/components/callbacks/checkpoint.py
@@ -5,8 +5,8 @@ from typing import Any, Dict, Optional
 from artifact_experiment.base.tracking.client import TrackingClient
 
 from artifact_torch.base.components.callbacks.periodic import (
+    PeriodicCallback,
     PeriodicCallbackResources,
-    PeriodicTrackingCallback,
 )
 
 
@@ -15,23 +15,19 @@ class CheckpointCallbackResources(PeriodicCallbackResources):
     checkpoint: Dict[str, Any]
 
 
-class CheckpointCallback(
-    PeriodicTrackingCallback[
-        CheckpointCallbackResources,
-        Dict[str, Any],
-    ],
-):
+class CheckpointCallback(PeriodicCallback[CheckpointCallbackResources]):
     def __init__(self, period: int, tracking_client: Optional[TrackingClient] = None):
-        key = self._get_key()
-        super().__init__(key=key, period=period, tracking_client=tracking_client)
+        super().__init__(period=period)
+        self._tracking_client = tracking_client
 
     @staticmethod
     @abstractmethod
-    def _export(key: str, value: Dict[str, Any], tracking_client: TrackingClient): ...
+    def _export(checkpoint: Dict[str, Any], tracking_client: TrackingClient, step: int): ...
 
-    @classmethod
-    def _get_key(cls) -> str:
-        return "checkpoint"
-
-    def _compute(self, resources: CheckpointCallbackResources) -> Dict[str, Any]:
-        return resources.checkpoint
+    def _execute(self, resources: CheckpointCallbackResources):
+        if self._tracking_client is not None:
+            self._export(
+                checkpoint=resources.checkpoint,
+                tracking_client=self._tracking_client,
+                step=resources.step,
+            )
