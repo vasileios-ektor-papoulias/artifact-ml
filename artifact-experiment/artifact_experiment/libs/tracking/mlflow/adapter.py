cb27c3d8b6acaa91aad68c01fb7e6b3609e2fd92
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-06-06 12:02:49 +0300

    Direct outputs to designated dir in mlflow logging

--- artifact-experiment/artifact_experiment/libs/tracking/mlflow/adapter.py
+++ artifact-experiment/artifact_experiment/libs/tracking/mlflow/adapter.py
@@ -19,8 +19,9 @@ class MlflowNativeClient:
 
 
 class MlflowRunAdapter(RunAdapter[MlflowNativeClient]):
-    DEFAULT_TRACKING_URI = "http://localhost:5000"
-    TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI", DEFAULT_TRACKING_URI)
+    _root_dir = "artifact_ml"
+    _default_tracking_uri = "http://localhost:5000"
+    TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI", _default_tracking_uri)
 
     @property
     def experiment_id(self) -> str:
@@ -53,6 +54,7 @@ class MlflowRunAdapter(RunAdapter[MlflowNativeClient]):
     def upload(self, path_source: str, dir_target: str):
         if not self.is_active:
             raise InactiveMlflowRunError("Run is inactive")
+        path_source = self._prepend_root_dir(path=path_source)
         self._native_run.client.log_artifact(
             run_id=self.run_uuid,
             local_path=path_source,
@@ -62,16 +64,19 @@ class MlflowRunAdapter(RunAdapter[MlflowNativeClient]):
     def log_score(self, backend_path: str, value: float, step: int = 0):
         if not self.is_active:
             raise InactiveMlflowRunError("Run is inactive")
+        backend_path = self._prepend_root_dir(path=backend_path)
         key = backend_path.replace("\\", "/")
         self._native_run.client.log_metric(run_id=self.run_uuid, key=key, value=value, step=step)
 
     def get_ls_artifact_info(self, backend_path: str) -> List[FileInfo]:
+        backend_path = self._prepend_root_dir(path=backend_path)
         ls_artifact_infos = self._native_run.client.list_artifacts(
             run_id=self.run_uuid, path=backend_path
         )
         return ls_artifact_infos
 
     def get_ls_score_history(self, backend_path: str) -> List[Metric]:
+        backend_path = self._prepend_root_dir(path=backend_path)
         key = backend_path.replace("\\", "/")
         ls_metric_history = self._native_run.client.get_metric_history(
             run_id=self.run_uuid, key=key
@@ -116,3 +121,7 @@ class MlflowRunAdapter(RunAdapter[MlflowNativeClient]):
             run_info = ls_runs[0].info
             run = mlflow_client.get_run(run_id=run_info.run_id)
             return run
+
+    @classmethod
+    def _prepend_root_dir(cls, path: str) -> str:
+        return os.path.join(cls._root_dir, path)
