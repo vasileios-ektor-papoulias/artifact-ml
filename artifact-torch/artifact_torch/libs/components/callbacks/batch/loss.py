c3dd15be1bb90d4dc46d78e583bdc75951c483c8
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-03 06:50:39 +0300

    Adapt loss callback

--- artifact-torch/artifact_torch/libs/components/callbacks/batch/loss.py
+++ artifact-torch/artifact_torch/libs/components/callbacks/batch/loss.py
@@ -1,17 +1,21 @@
 from artifact_torch.base.components.callbacks.batch import (
     BatchScoreCallback,
 )
+from artifact_torch.base.model.base import Model
 from artifact_torch.base.model.io import ModelInput, ModelOutput
 
 
-class BatchLossCallback(BatchScoreCallback[ModelInput, ModelOutput]):
+class BatchLossCallback(BatchScoreCallback[ModelInput, ModelOutput, Model]):
     @classmethod
     def _get_key(cls):
         return "BATCH_LOSS"
 
     @staticmethod
-    def _compute_on_batch(model_input: ModelInput, model_output: ModelOutput) -> float:
+    def _compute_on_batch(
+        model_input: ModelInput, model_output: ModelOutput, model: Model
+    ) -> float:
         _ = model_input
+        _ = model
         t_loss = model_output.get("t_loss")
         assert t_loss is not None
         return t_loss.item()
