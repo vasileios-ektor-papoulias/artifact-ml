7554d0e3c1fba2c75ca4c8b1ae894fb6d79f2f3f
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-03 05:49:21 +0300

    Add paths to artifact dirs in config

--- artifact-core/artifact_core/table_comparison/config/parsed.py
+++ artifact-core/artifact_core/table_comparison/config/parsed.py
@@ -1,4 +1,5 @@
 import json
+import os
 from pathlib import Path
 from typing import Any, Dict
 
@@ -8,6 +9,7 @@ from artifact_core.libs.utils.config_utils import (
     EngineConfigType,
 )
 
+ARTIFACT_CORE_ROOT = Path(__file__).resolve().parent.parent.parent
 CONFIG_DIR = Path(__file__).resolve().parent
 CONFIG_FILE = CONFIG_DIR / "raw.json"
 
@@ -22,6 +24,11 @@ _merged_artifact_configs = ConfigMerger.merge(
     base_config=_dict_artifact_configs, override=_user_override
 )
 
+
+NATIVE_ARTIFACT_PATH = os.path.join(
+    ARTIFACT_CORE_ROOT, _merged_artifact_configs.get("native_artifact_path", "")
+)
+CUSTOM_ARTIFACT_PATH = _merged_artifact_configs.get("custom_artifact_path", "")
 DICT_SCORES_CONFIG = _merged_artifact_configs.get("scores", {})
 DICT_ARRAYS_CONFIG = _merged_artifact_configs.get("arrays", {})
 DICT_PLOTS_CONFIG = _merged_artifact_configs.get("plots", {})
