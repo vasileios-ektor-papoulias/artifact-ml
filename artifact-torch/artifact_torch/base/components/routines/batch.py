99e4aee42109a37ae8fe4c8b63da84b571e07b5e
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-03 06:50:25 +0300

    Adapt batch routine

--- artifact-torch/artifact_torch/base/components/routines/batch.py
+++ artifact-torch/artifact_torch/base/components/routines/batch.py
@@ -8,17 +8,19 @@ from artifact_torch.base.components.callbacks.batch import (
     BatchCallbackHandler,
     BatchCallbackResources,
 )
+from artifact_torch.base.model.base import Model
 from artifact_torch.base.model.io import ModelInput, ModelOutput
 
 ModelInputTContr = TypeVar("ModelInputTContr", bound=ModelInput, contravariant=True)
 ModelOutputTContr = TypeVar("ModelOutputTContr", bound=ModelOutput, contravariant=True)
+ModelTContr = TypeVar("ModelTContr", bound=Model, contravariant=True)
 BatchRoutineT = TypeVar("BatchRoutineT", bound="BatchRoutine")
 
 
-class BatchRoutine(ABC, Generic[ModelInputTContr, ModelOutputTContr]):
+class BatchRoutine(ABC, Generic[ModelInputTContr, ModelOutputTContr, ModelTContr]):
     def __init__(
         self,
-        handler: BatchCallbackHandler[ModelInputTContr, ModelOutputTContr, Any],
+        handler: BatchCallbackHandler[ModelInputTContr, ModelOutputTContr, ModelTContr, Any],
     ):
         self._handler = handler
 
@@ -38,22 +40,26 @@ class BatchRoutine(ABC, Generic[ModelInputTContr, ModelOutputTContr]):
     @abstractmethod
     def _get_batch_callbacks(
         tracking_client: Optional[TrackingClient],
-    ) -> List[BatchCallback[ModelInputTContr, ModelOutputTContr, Any]]: ...
+    ) -> List[BatchCallback[ModelInputTContr, ModelOutputTContr, ModelTContr, Any]]: ...
 
     def execute(
-        self, model_input: ModelInputTContr, model_output: ModelOutputTContr, batch_idx: int
+        self,
+        model_input: ModelInputTContr,
+        model_output: ModelOutputTContr,
+        model: ModelTContr,
+        batch_idx: int,
     ):
-        resources = BatchCallbackResources[ModelInputTContr, ModelOutputTContr](
-            step=batch_idx, model_input=model_input, model_output=model_output
+        resources = BatchCallbackResources[ModelInputTContr, ModelOutputTContr, ModelTContr](
+            step=batch_idx, model_input=model_input, model_output=model_output, model=model
         )
         self._handler.execute(resources=resources)
 
     @classmethod
     def _build(
         cls: Type[BatchRoutineT],
-        ls_callbacks: List[BatchCallback[ModelInputTContr, ModelOutputTContr, Any]],
+        ls_callbacks: List[BatchCallback[ModelInputTContr, ModelOutputTContr, ModelTContr, Any]],
     ) -> BatchRoutineT:
-        handler = BatchCallbackHandler[ModelInputTContr, ModelOutputTContr, Any](
+        handler = BatchCallbackHandler[ModelInputTContr, ModelOutputTContr, ModelTContr, Any](
             ls_callbacks=ls_callbacks,
         )
         subroutine = cls(
