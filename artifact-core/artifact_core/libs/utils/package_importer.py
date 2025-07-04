385ccc3a07a91477dd4bdbae6d61a099bf8597d4
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-04 14:47:49 +0300

    fIX IMPORT ISSUE

--- artifact-core/artifact_core/libs/utils/package_importer.py
+++ artifact-core/artifact_core/libs/utils/package_importer.py
@@ -3,16 +3,35 @@ import os
 import pkgutil
 import sys
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 
 class PackageImporter:
-    @staticmethod
-    def import_all_from_package_path(path: Union[Path, str]):
+    @classmethod
+    def import_all_from_package_path(
+        cls, path: Union[Path, str], root: Optional[Union[Path, str]] = None
+    ):
         path = os.path.abspath(path)
-        package_name = os.path.basename(path)
-        parent_dir = os.path.dirname(path)
+        package_name = cls._get_package_name(path, root)
+        parent_dir = cls._get_parent_directory(path, root)
         if parent_dir not in sys.path:
             sys.path.insert(0, parent_dir)
         for _, module_name, _ in pkgutil.walk_packages([path], prefix=package_name + "."):
             importlib.import_module(module_name)
+
+    @staticmethod
+    def _get_package_name(path: str, root: Optional[Union[Path, str]] = None) -> str:
+        if root is not None:
+            root = os.path.abspath(root)
+            relative_path = os.path.relpath(path, root)
+            root_name = os.path.basename(root)
+            return f"{root_name}.{relative_path.replace(os.sep, '.')}"
+        else:
+            return os.path.basename(path)
+
+    @staticmethod
+    def _get_parent_directory(path: str, root: Optional[Union[Path, str]] = None) -> str:
+        if root is not None:
+            return os.path.dirname(os.path.abspath(root))
+        else:
+            return os.path.dirname(path)
--- artifact-core/artifact_core/table_comparison/__init__.py
+++ artifact-core/artifact_core/table_comparison/__init__.py
@@ -1,6 +1,10 @@
 from artifact_core.libs.resource_spec.tabular.spec import TabularDataSpec
 from artifact_core.libs.utils.package_importer import PackageImporter
-from artifact_core.table_comparison.config.parsed import CUSTOM_ARTIFACT_PATH, NATIVE_ARTIFACT_PATH
+from artifact_core.table_comparison.config.parsed import (
+    ARTIFACT_CORE_ROOT,
+    CUSTOM_ARTIFACT_PATH,
+    NATIVE_ARTIFACT_PATH,
+)
 from artifact_core.table_comparison.engine.engine import (
     TableComparisonArrayCollectionType,
     TableComparisonArrayType,
@@ -11,5 +15,17 @@ from artifact_core.table_comparison.engine.engine import (
     TableComparisonScoreType,
 )
 
-PackageImporter.import_all_from_package_path(path=NATIVE_ARTIFACT_PATH)
+# Define public API
+__all__ = [
+    "TabularDataSpec",
+    "TableComparisonArrayCollectionType",
+    "TableComparisonArrayType",
+    "TableComparisonEngine",
+    "TableComparisonPlotCollectionType",
+    "TableComparisonPlotType",
+    "TableComparisonScoreCollectionType",
+    "TableComparisonScoreType",
+]
+
+PackageImporter.import_all_from_package_path(path=NATIVE_ARTIFACT_PATH, root=ARTIFACT_CORE_ROOT)
 PackageImporter.import_all_from_package_path(path=CUSTOM_ARTIFACT_PATH)
