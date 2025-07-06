6172a9bb2df362baedf1d283db1838d2eb6e1e23
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-06 16:06:12 +0300

    Add accessors to adapter

--- artifact-experiment/artifact_experiment/libs/tracking/in_memory/adapter.py
+++ artifact-experiment/artifact_experiment/libs/tracking/in_memory/adapter.py
@@ -1,4 +1,7 @@
-from typing import Dict
+from typing import Dict, List
+
+from matplotlib.figure import Figure
+from numpy import ndarray
 
 from artifact_experiment.base.tracking.adapter import RunAdapter
 from artifact_experiment.libs.tracking.in_memory.native_run import (
@@ -23,17 +26,77 @@ class InMemoryTrackingAdapter(RunAdapter[InMemoryNativeRun]):
     def uploaded_files(self):
         return self._native_run.uploaded_files
 
-    @classmethod
-    def _build_native_run(cls, experiment_id: str, run_id: str) -> InMemoryNativeRun:
-        return InMemoryNativeRun(experiment_id=experiment_id, run_id=run_id)
+    @property
+    def dict_scores(self) -> Dict[str, float]:
+        return self._native_run.dict_scores
 
-    def _start(self, run_id: str):
-        if self.run_id != run_id:
-            self._native_run = self._build_native_run(
-                experiment_id=self.experiment_id, run_id=run_id
-            )
-        else:
-            self._native_run.is_active = True
+    @property
+    def ls_scores(self) -> List[float]:
+        return list(self.dict_scores.values())
+
+    @property
+    def n_scores(self) -> int:
+        return len(self.ls_scores)
+
+    @property
+    def dict_arrays(self) -> Dict[str, ndarray]:
+        return self._native_run.dict_arrays
+
+    @property
+    def ls_arrays(self) -> List[ndarray]:
+        return list(self.dict_arrays.values())
+
+    @property
+    def n_arrays(self) -> int:
+        return len(self.ls_arrays)
+
+    @property
+    def dict_plots(self) -> Dict[str, Figure]:
+        return self._native_run.dict_plots
+
+    @property
+    def ls_plots(self) -> List[Figure]:
+        return list(self.dict_plots.values())
+
+    @property
+    def n_plots(self) -> int:
+        return len(self.ls_plots)
+
+    @property
+    def dict_score_collections(self) -> Dict[str, Dict[str, float]]:
+        return self._native_run.dict_score_collections
+
+    @property
+    def ls_score_collections(self) -> List[Dict[str, float]]:
+        return list(self.dict_score_collections.values())
+
+    @property
+    def n_score_collections(self) -> int:
+        return len(self.ls_score_collections)
+
+    @property
+    def dict_array_collections(self) -> Dict[str, Dict[str, ndarray]]:
+        return self._native_run.dict_array_collections
+
+    @property
+    def ls_array_collections(self) -> List[Dict[str, ndarray]]:
+        return list(self.dict_array_collections.values())
+
+    @property
+    def n_array_collections(self) -> int:
+        return len(self.ls_array_collections)
+
+    @property
+    def dict_plot_collections(self) -> Dict[str, Dict[str, Figure]]:
+        return self._native_run.dict_plot_collections
+
+    @property
+    def ls_plot_collections(self) -> List[Dict[str, Figure]]:
+        return list(self.dict_plot_collections.values())
+
+    @property
+    def n_plot_collections(self) -> int:
+        return len(self.ls_plot_collections)
 
     def stop(self):
         self._native_run.is_active = False
@@ -46,3 +109,15 @@ class InMemoryTrackingAdapter(RunAdapter[InMemoryNativeRun]):
     def _format_upload_entry(path_source: str, dir_target: str) -> Dict[str, str]:
         dict_upload_entry = {"path_source": path_source, "dir_target": dir_target}
         return dict_upload_entry
+
+    @classmethod
+    def _build_native_run(cls, experiment_id: str, run_id: str) -> InMemoryNativeRun:
+        return InMemoryNativeRun(experiment_id=experiment_id, run_id=run_id)
+
+    def _start(self, run_id: str):
+        if self.run_id != run_id:
+            self._native_run = self._build_native_run(
+                experiment_id=self.experiment_id, run_id=run_id
+            )
+        else:
+            self._native_run.is_active = True
