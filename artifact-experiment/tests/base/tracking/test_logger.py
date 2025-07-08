f3b15fcc9ac12477fe3638b14384465ddabd69f3
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-08 15:47:41 +0300

    Test base logger

---
+++ artifact-experiment/tests/base/tracking/test_logger.py
@@ -0,0 +1,39 @@
+from typing import Callable, List, Optional, Tuple
+
+import pytest
+from artifact_core.base.artifact_dependencies import ArtifactResult
+from pytest_mock import MockerFixture
+
+from tests.base.tracking.dummy.adapter import DummyRunAdapter
+from tests.base.tracking.dummy.logger import DummyArtifactLogger
+
+
+@pytest.mark.parametrize(
+    "ls_artifacts",
+    [
+        ([]),
+        (["score_1"]),
+        (["score_1", "score_2"]),
+        (["score_1", "score_3"]),
+        (["score_1", "score_2", "score_3"]),
+        (["score_1", "score_2", "score_3", "score_4", "score_5"]),
+        (["score_1", "array_2", "plot_1", "plot_collection_4"]),
+    ],
+    indirect=True,
+)
+def test_log(
+    mocker: MockerFixture,
+    logger_factory: Callable[
+        [Optional[DummyRunAdapter]], Tuple[DummyRunAdapter, DummyArtifactLogger]
+    ],
+    ls_artifacts: List[ArtifactResult],
+):
+    adapter, logger = logger_factory(None)
+    print(type(adapter))
+    adapter.log = mocker.MagicMock()
+    for idx, artifact in enumerate(ls_artifacts):
+        logger.log(artifact_name="artifact", artifact=artifact)
+    assert adapter.log.call_count == len(ls_artifacts)
+    for idx, call_args in enumerate(adapter.log.call_args_list):
+        artifact = ls_artifacts[idx]
+        assert call_args.kwargs == {"artifact_path": "", "artifact": artifact}
