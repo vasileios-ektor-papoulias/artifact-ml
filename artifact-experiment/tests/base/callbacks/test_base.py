9f6caed95300ed48a4e6b644386b0bb934731926
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-22 23:53:40 +0300

    Adapt base callback test pattern

--- artifact-experiment/tests/base/callbacks/test_base.py
+++ artifact-experiment/tests/base/callbacks/test_base.py
@@ -1,3 +1,5 @@
+from typing import Callable, Literal
+
 import pytest
 
 from tests.base.callbacks.dummy.base import (
@@ -7,16 +9,51 @@ from tests.base.callbacks.dummy.base import (
     MultiplyCallback,
 )
 
+CallbackType = Literal["add", "multiply"]
+
+
+@pytest.fixture
+def resources_factory() -> Callable[[float, float], DummyCallbackResources]:
+    def _factory(x: float, y: float) -> DummyCallbackResources:
+        return DummyCallbackResources(x=x, y=y)
+
+    return _factory
+
+
+@pytest.fixture
+def callback_factory() -> Callable[[CallbackType], DummyCallback]:
+    def _factory(callback_type: CallbackType) -> DummyCallback:
+        if callback_type == "add":
+            return AddCallback()
+        elif callback_type == "multiply":
+            return MultiplyCallback()
+        else:
+            raise ValueError(f"Unknown callback type: {callback_type}")
+
+    return _factory
+
 
 @pytest.mark.parametrize(
-    "callback, resources, expected_result",
+    "callback_type, x, y, expected_result",
     [
-        (AddCallback(), DummyCallbackResources(x=1, y=0), 1),
-        (MultiplyCallback(), DummyCallbackResources(x=1, y=0), 0),
+        ("add", 1, 0, 1),
+        ("add", 1, -1, 0),
+        ("add", 1, 1, 2),
+        ("multiply", 1, 0, 0),
+        ("multiply", 1, 1, 1),
+        ("multiply", 1, -1, -1),
+        ("multiply", 2, 2, 4),
     ],
 )
 def test_callback(
-    callback: DummyCallback, resources: DummyCallbackResources, expected_result: float
+    resources_factory: Callable[[float, float], DummyCallbackResources],
+    callback_factory: Callable[[CallbackType], DummyCallback],
+    callback_type: CallbackType,
+    x: float,
+    y: float,
+    expected_result: float,
 ):
+    resources = resources_factory(x, y)
+    callback = callback_factory(callback_type)
     callback.execute(resources=resources)
     assert callback.result == expected_result
