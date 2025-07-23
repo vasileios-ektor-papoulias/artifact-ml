5c953f9df87c0c21be957d02f1e9522e14bbfc39
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-24 00:36:39 +0300

    Test filesystem score collection logger

--- artifact-experiment/tests/libs/tracking/filesystem/loggers/test_score_collections.py
+++ artifact-experiment/tests/libs/tracking/filesystem/loggers/test_score_collections.py
@@ -1,77 +1,74 @@
-# import os
-# from typing import Callable, Dict, List, Optional, Tuple
+import os
+from typing import Callable, Dict, List, Optional, Tuple
 
-# import pytest
-# from artifact_experiment.libs.tracking.neptune.adapter import NeptuneRunAdapter
-# from artifact_experiment.libs.tracking.neptune.loggers.score_collections import (
-#     NeptuneScoreCollectionLogger,
-# )
-# from pytest_mock import MockerFixture
+import pandas as pd
+import pytest
+from artifact_experiment.libs.tracking.filesystem.adapter import FilesystemRunAdapter
+from artifact_experiment.libs.tracking.filesystem.loggers.score_collections import (
+    FilesystemScoreCollectionLogger,
+)
 
 
-# @pytest.mark.parametrize(
-#     "experiment_id, run_id, ls_score_collection_names, ls_score_collections",
-#     [
-#         ("exp1", "run1", [], []),
-#         ("exp1", "run1", ["score_collection_1"], ["score_collection_1"]),
-#         (
-#             "exp1",
-#             "run1",
-#             ["score_collection_1", "score_collection_2"],
-#             ["score_collection_1", "score_collection_2"],
-#         ),
-#         (
-#             "exp1",
-#             "run1",
-#             ["score_collection_1", "score_collection_3"],
-#             ["score_collection_1", "score_collection_3"],
-#         ),
-#         (
-#             "exp1",
-#             "run1",
-#             ["score_collection_1", "score_collection_2", "score_collection_3"],
-#             ["score_collection_1", "score_collection_2", "score_collection_3"],
-#         ),
-#         (
-#             "exp1",
-#             "run1",
-#             [
-#                 "score_collection_1",
-#                 "score_collection_2",
-#                 "score_collection_3",
-#                 "score_collection_4",
-#                 "score_collection_5",
-#             ],
-#             [
-#                 "score_collection_1",
-#                 "score_collection_2",
-#                 "score_collection_3",
-#                 "score_collection_4",
-#                 "score_collection_5",
-#             ],
-#         ),
-#     ],
-#     indirect=["ls_score_collections"],
-# )
-# def test_log(
-#     mocker: MockerFixture,
-#     score_collection_logger_factory: Callable[
-#         [Optional[str], Optional[str]], Tuple[NeptuneRunAdapter, NeptuneScoreCollectionLogger]
-#     ],
-#     experiment_id: str,
-#     run_id: str,
-#     ls_score_collection_names: List[str],
-#     ls_score_collections: List[Dict[str, float]],
-# ):
-#     adapter, logger = score_collection_logger_factory(experiment_id, run_id)
-#     adapter.log = mocker.MagicMock()
-#     for score_collection_name, score_collection in zip(
-#         ls_score_collection_names, ls_score_collections
-#     ):
-#         logger.log(artifact_name=score_collection_name, artifact=score_collection)
-#     assert adapter.log.call_count == len(ls_score_collections)
-#     for idx, call_args in enumerate(adapter.log.call_args_list):
-#         score_collection_name = ls_score_collection_names[idx]
-#         score_collection = ls_score_collections[idx]
-#         expected_path = os.path.join("artifacts", "score_collections", score_collection_name)
-#         assert call_args.kwargs == {"artifact_path": expected_path, "artifact": score_collection}
+@pytest.mark.parametrize(
+    "experiment_id, run_id, ls_score_collection_names, ls_score_collections, expected_logs",
+    [
+        ("exp1", "run1", [], [], {}),
+        (
+            "exp1",
+            "run1",
+            ["score_collection_key_1"],
+            ["score_collection_1"],
+            {"score_collection_key_1": ["score_collection_1"]},
+        ),
+        (
+            "exp1",
+            "run1",
+            ["score_collection_key_1", "score_collection_key_2"],
+            ["score_collection_1", "score_collection_2"],
+            {
+                "score_collection_key_1": ["score_collection_1"],
+                "score_collection_key_2": ["score_collection_2"],
+            },
+        ),
+        (
+            "exp1",
+            "run1",
+            ["score_collection_key_1", "score_collection_key_1"],
+            ["score_collection_1", "score_collection_2"],
+            {"score_collection_key_1": ["score_collection_1", "score_collection_2"]},
+        ),
+        (
+            "exp1",
+            "run1",
+            ["score_collection_key_1", "score_collection_key_2", "score_collection_key_1"],
+            ["score_collection_1", "score_collection_2", "score_collection_3"],
+            {
+                "score_collection_key_1": ["score_collection_1", "score_collection_3"],
+                "score_collection_key_2": ["score_collection_2"],
+            },
+        ),
+    ],
+    indirect=["ls_score_collections", "expected_logs"],
+)
+def test_log_score_collection(
+    in_memory_df_store: Dict[str, pd.DataFrame],
+    score_collection_logger_factory: Callable[
+        [Optional[str], Optional[str]],
+        Tuple[FilesystemRunAdapter, FilesystemScoreCollectionLogger],
+    ],
+    experiment_id: str,
+    run_id: str,
+    ls_score_collection_names: List[str],
+    ls_score_collections: List[Dict[str, float]],
+    expected_logs: Dict[str, List[Dict[str, float]]],
+):
+    _, logger = score_collection_logger_factory(experiment_id, run_id)
+    for name, score_dict in zip(ls_score_collection_names, ls_score_collections):
+        logger.log(artifact_name=name, artifact=score_dict)
+    for name, expected_rows in expected_logs.items():
+        path = os.path.join(
+            "test_root", experiment_id, run_id, "artifacts", "score_collections", name
+        )
+        df = in_memory_df_store[path]
+        expected_df = pd.DataFrame(expected_rows)
+        pd.testing.assert_frame_equal(df.reset_index(drop=True), expected_df.reset_index(drop=True))
