71abba2a5f641e0580da519d16aa77879052cfef
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-23 20:21:36 +0300

    Test filesystem native run

---
+++ artifact-experiment/tests/libs/tracking/filesystem/test_native_run.py
@@ -0,0 +1,37 @@
+import os
+from typing import Callable, Optional
+
+import pytest
+from artifact_experiment.libs.tracking.filesystem.native_run import FilesystemRun
+
+
+@pytest.mark.parametrize(
+    "experiment_id, run_id",
+    [("exp1", "run1")],
+)
+def test_init(
+    experiment_id: str,
+    run_id: str,
+    native_run_factory: Callable[[Optional[str], Optional[str]], FilesystemRun],
+):
+    native_run = native_run_factory(experiment_id, run_id)
+    assert native_run.experiment_id == experiment_id
+    assert native_run.run_id == run_id
+    assert native_run.is_active is True
+    assert native_run.experiment_dir == os.path.join("test_root", experiment_id)
+    assert native_run.run_dir == os.path.join("test_root", experiment_id, run_id)
+
+
+@pytest.mark.parametrize(
+    "experiment_id, run_id",
+    [("exp1", "run1")],
+)
+def test_stop(
+    native_run_factory: Callable[[Optional[str], Optional[str]], FilesystemRun],
+    experiment_id: str,
+    run_id: str,
+):
+    native_run = native_run_factory(experiment_id, run_id)
+    assert native_run.is_active is True
+    native_run.stop()
+    assert native_run.is_active is False
