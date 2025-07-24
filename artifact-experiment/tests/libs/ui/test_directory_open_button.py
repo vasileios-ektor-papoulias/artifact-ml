b3bb3cc27853a61e734fb55e1b22fab74dd3d447
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-24 15:11:15 +0300

    Test directory open button

---
+++ artifact-experiment/tests/libs/ui/test_directory_open_button.py
@@ -0,0 +1,20 @@
+import pytest
+from artifact_experiment.libs.ui.directory_open_button import DirectoryOpenButton
+from pytest_mock import MockerFixture
+
+
+@pytest.mark.parametrize(
+    "path, description",
+    [
+        ("/some/path", "Open Directory"),
+        ("C:\\windows\\path", "Open Windows Folder"),
+    ],
+)
+def test_button(mocker: MockerFixture, path: str, description: str):
+    mock_dir_opener = mocker.patch(
+        "artifact_experiment.libs.ui.directory_open_button.DirectoryOpener"
+    )
+    btn = DirectoryOpenButton(path=path, description=description)
+    assert btn.button.description == description
+    btn.click()
+    mock_dir_opener.open_directory.assert_called_once_with(path=path)
