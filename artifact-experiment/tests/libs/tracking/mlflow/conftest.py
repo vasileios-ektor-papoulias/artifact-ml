063df122268a20c51d576662ac5195f62851ae38
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-26 14:42:57 +0300

    Patch env var tracking uri recovery in mlflow tracking unit tests

--- artifact-experiment/tests/libs/tracking/mlflow/conftest.py
+++ artifact-experiment/tests/libs/tracking/mlflow/conftest.py
@@ -25,103 +25,112 @@ from pytest_mock import MockerFixture
 
 
 @pytest.fixture
-def mock_client_factory(mocker) -> Callable[[], MagicMock]:
-    def _factory() -> MagicMock:
-        experiments = {}
-        runs = {}
-        client = mocker.MagicMock(spec=MlflowClient)
-
-        def get_experiment_by_name(name: str):
-            return experiments.get(name)
-
-        def get_experiment(experiment_id: str):
-            for exp in experiments.values():
-                if exp.experiment_id == experiment_id:
-                    return exp
-            return None
-
-        def create_experiment(name: str):
-            if name in experiments:
-                return experiments[name].experiment_id
-            exp = mocker.MagicMock()
-            exp.name = name
-            exp.experiment_id = f"{name}_uuid"
-            experiments[name] = exp
-            return exp.experiment_id
-
-        def search_runs(experiment_ids, filter_string=None):
-            exp_id = experiment_ids[0]
-            if not filter_string:
-                return list(runs.get(exp_id, {}).values())
-            if "tags.mlflow.runName" in filter_string:
-                name = filter_string.split("=")[-1].strip(" '\"")
-                return [run for run in runs.get(exp_id, {}).values() if run.info.run_name == name]
-            return []
-
-        def get_run(run_id: str):
-            for exp_runs in runs.values():
-                if run_id in exp_runs:
-                    return exp_runs[run_id]
-            raise ValueError("Run not found")
-
-        def create_run(experiment_id: str, run_name: str):
-            run_uuid = f"{run_name}_uuid"
-            run = mocker.MagicMock(spec=Run)
-            info = mocker.MagicMock()
-            info.run_id = run_uuid
-            info.experiment_id = experiment_id
-            info.run_name = run_name
-            info.status = RunStatus.to_string(RunStatus.RUNNING)
-            run.info = info
-            run.data = mocker.MagicMock()
-            runs.setdefault(experiment_id, {})[run_uuid] = run
-            return run
-
-        client.get_experiment_by_name.side_effect = get_experiment_by_name
-        client.get_experiment.side_effect = get_experiment
-        client.create_experiment.side_effect = create_experiment
-        client.search_runs.side_effect = search_runs
-        client.get_run.side_effect = get_run
-        client.create_run.side_effect = create_run
-
-        return client
-
-    return _factory
-
-
-@pytest.fixture
-def mock_experiment_factory(mocker) -> Callable[[str], MagicMock]:
+def mock_experiment_factory(mocker: MockerFixture) -> Callable[[str], MagicMock]:
     def _factory(name: str) -> MagicMock:
-        experiment = mocker.MagicMock()
-        experiment.name = name
-        experiment.experiment_id = f"{name}_uuid"
-        return experiment
+        mock_experiment = mocker.MagicMock()
+        mock_experiment.name = name
+        mock_experiment.experiment_id = f"{name}_uuid"
+        return mock_experiment
 
     return _factory
 
 
 @pytest.fixture
-def mock_run_factory(mocker) -> Callable[[str, str], MagicMock]:
+def mock_run_factory(mocker: MockerFixture) -> Callable[[str, str], MagicMock]:
     def _factory(experiment_name: str, run_name: str) -> MagicMock:
         run_uuid = f"{run_name}_uuid"
         experiment_uuid = f"{experiment_name}_uuid"
-        run_info = mocker.MagicMock()
-        run_info.run_id = run_uuid
-        run_info.experiment_id = experiment_uuid
-        run_info.run_name = run_name
-        run_info.status = RunStatus.to_string(RunStatus.RUNNING)
-        raw_run = mocker.MagicMock(spec=Run)
-        raw_run.info = run_info
-        raw_run.data = mocker.MagicMock()
+        mock_run_info = mocker.MagicMock()
+        mock_run_info.run_id = run_uuid
+        mock_run_info.experiment_id = experiment_uuid
+        mock_run_info.run_name = run_name
+        mock_run_info.status = RunStatus.to_string(RunStatus.RUNNING)
+        mock_run = mocker.MagicMock(spec=Run)
+        mock_run.info = mock_run_info
+        mock_run.data = mocker.MagicMock()
 
-        return raw_run
+        return mock_run
 
     return _factory
+
+
+@pytest.fixture
+def mock_client(
+    mocker: MockerFixture,
+    mock_experiment_factory: Callable[[str], MagicMock],
+    mock_run_factory: Callable[[str, str], MagicMock],
+) -> MagicMock:
+    experiments = {}
+    runs = {}
+
+    def get_experiment_by_name(name: str):
+        return experiments.get(name)
+
+    def get_experiment(experiment_id: str):
+        return next(
+            (exp for exp in experiments.values() if exp.experiment_id == experiment_id),
+            None,
+        )
+
+    def create_experiment(name: str):
+        if name in experiments:
+            return experiments[name].experiment_id
+        exp = mock_experiment_factory(name)
+        experiments[name] = exp
+        return exp.experiment_id
+
+    def search_runs(experiment_ids, filter_string=None):
+        exp_id = experiment_ids[0]
+        if not filter_string:
+            return list(runs.get(exp_id, {}).values())
+
+        if "tags.mlflow.runName" in filter_string:
+            name = filter_string.split("=")[-1].strip(" '\"")
+            return [run for run in runs.get(exp_id, {}).values() if run.info.run_name == name]
+        return []
+
+    def get_run(run_id: str):
+        for exp_runs in runs.values():
+            if run_id in exp_runs:
+                return exp_runs[run_id]
+        raise ValueError("Run not found")
+
+    def create_run(experiment_id: str, run_name: str):
+        exp = get_experiment(experiment_id)
+        if exp is None:
+            raise ValueError(f"Experiment ID {experiment_id} not found")
+
+        run = mock_run_factory(exp.name, run_name)
+        runs.setdefault(experiment_id, {})[run.info.run_id] = run
+        return run
+
+    mock_client = mocker.MagicMock(spec=MlflowClient)
+    mock_client.get_experiment_by_name.side_effect = get_experiment_by_name
+    mock_client.get_experiment.side_effect = get_experiment
+    mock_client.create_experiment.side_effect = create_experiment
+    mock_client.search_runs.side_effect = search_runs
+    mock_client.get_run.side_effect = get_run
+    mock_client.create_run.side_effect = create_run
+
+    return mock_client
+
+
+@pytest.fixture
+def patch_mlflow_run_creation(mocker: MockerFixture, mock_client: MagicMock):
+    mocker.patch(
+        "artifact_experiment.libs.utils.environment_variable_reader.EnvironmentVariableReader.get",
+        return_value="dummy-tracking_uri",
+    )
+    mocker.patch(
+        "artifact_experiment.libs.tracking.mlflow.adapter.MlflowClient",
+        return_value=mock_client,
+    )
 
 
 @pytest.fixture
 def native_run_factory(
-    mock_client_factory: Callable[[], MagicMock],
+    patch_mlflow_run_creation,
+    mock_client: MagicMock,
 ) -> Callable[[Optional[str], Optional[str]], MlflowNativeRun]:
     def _factory(
         experiment_id: Optional[str] = None,
@@ -129,29 +138,12 @@ def native_run_factory(
     ) -> MlflowNativeRun:
         experiment_id = experiment_id or "default_experiment"
         run_id = run_id or "default_run"
-        client = mock_client_factory()
-        client.create_experiment(name=experiment_id)
-        experiment = client.get_experiment_by_name(name=experiment_id)
-        run = client.create_run(experiment_id=experiment.experiment_id, run_name=run_id)
-        return MlflowNativeRun(client=client, experiment=experiment, run=run)
+        mock_client.create_experiment(name=experiment_id)
+        experiment = mock_client.get_experiment_by_name(name=experiment_id)
+        run = mock_client.create_run(experiment_id=experiment.experiment_id, run_name=run_id)
+        return MlflowNativeRun(client=mock_client, experiment=experiment, run=run)
 
     return _factory
-
-
-@pytest.fixture
-def patch_mlflow_run_creation(
-    mocker: MockerFixture,
-    native_run_factory: Callable[[Optional[str], Optional[str]], MlflowNativeRun],
-):
-    def _patch(experiment_id: Optional[str] = None, run_id: Optional[str] = None):
-        native_run = native_run_factory(experiment_id, run_id)
-        mocker.patch(
-            "artifact_experiment.libs.tracking.mlflow.adapter.MlflowClient",
-            return_value=native_run.client,
-        )
-        return native_run
-
-    return _patch
 
 
 @pytest.fixture
