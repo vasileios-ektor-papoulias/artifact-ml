7865ed2cdbb8ab976e2223cdc2b09fdbf18251b5
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-07-28 17:48:57 +0300

    Test mlflow score logger

--- artifact-experiment/tests/libs/tracking/mlflow/loggers/test_scores.py
+++ artifact-experiment/tests/libs/tracking/mlflow/loggers/test_scores.py
@@ -1,46 +1,55 @@
-# import os
-# from typing import Callable, List, Optional, Tuple
+import os
+from typing import Callable, List, Optional, Tuple
 
-# import pytest
-# from artifact_experiment.libs.tracking.neptune.adapter import NeptuneRunAdapter
-# from artifact_experiment.libs.tracking.neptune.loggers.scores import NeptuneScoreLogger
-# from pytest_mock import MockerFixture
+import pytest
+from artifact_experiment.libs.tracking.mlflow.adapter import MlflowRunAdapter
+from artifact_experiment.libs.tracking.mlflow.loggers.scores import MlflowScoreLogger
+from pytest_mock import MockerFixture
 
 
-# @pytest.mark.parametrize(
-#     "experiment_id, run_id, ls_score_names, ls_scores",
-#     [
-#         ("exp1", "run1", [], []),
-#         ("exp1", "run1", ["score_1"], ["score_1"]),
-#         ("exp1", "run1", ["score_1", "score_2"], ["score_1", "score_2"]),
-#         ("exp1", "run1", ["score_1", "score_3"], ["score_1", "score_3"]),
-#         ("exp1", "run1", ["score_1", "score_2", "score_3"], ["score_1", "score_2", "score_3"]),
-#         (
-#             "exp1",
-#             "run1",
-#             ["score_1", "score_2", "score_3", "score_4", "score_5"],
-#             ["score_1", "score_2", "score_3", "score_4", "score_5"],
-#         ),
-#     ],
-#     indirect=["ls_scores"],
-# )
-# def test_log(
-#     mocker: MockerFixture,
-#     score_logger_factory: Callable[
-#         [Optional[str], Optional[str]], Tuple[NeptuneRunAdapter, NeptuneScoreLogger]
-#     ],
-#     experiment_id: str,
-#     run_id: str,
-#     ls_score_names: List[str],
-#     ls_scores: List[float],
-# ):
-#     adapter, logger = score_logger_factory(experiment_id, run_id)
-#     adapter.log = mocker.MagicMock()
-#     for score_name, score in zip(ls_score_names, ls_scores):
-#         logger.log(artifact_name=score_name, artifact=score)
-#     assert adapter.log.call_count == len(ls_scores)
-#     for idx, call_args in enumerate(adapter.log.call_args_list):
-#         score_name = ls_score_names[idx]
-#         score = ls_scores[idx]
-#         expected_path = os.path.join("artifacts", "scores", score_name)
-#         assert call_args.kwargs == {"artifact_path": expected_path, "artifact": score}
+@pytest.mark.parametrize(
+    "experiment_id, run_id, ls_score_names, ls_scores",
+    [
+        ("exp1", "run1", [], []),
+        ("exp1", "run1", ["score_1"], ["score_1"]),
+        ("exp1", "run1", ["score_1", "score_2"], ["score_1", "score_2"]),
+        ("exp1", "run1", ["score_1", "score_3"], ["score_1", "score_3"]),
+        ("exp1", "run1", ["score_1", "score_2", "score_3"], ["score_1", "score_2", "score_3"]),
+        (
+            "exp1",
+            "run1",
+            ["score_1", "score_2", "score_3", "score_4", "score_5"],
+            ["score_1", "score_2", "score_3", "score_4", "score_5"],
+        ),
+    ],
+    indirect=["ls_scores"],
+)
+def test_log(
+    mocker: MockerFixture,
+    score_logger_factory: Callable[
+        [Optional[str], Optional[str]], Tuple[MlflowRunAdapter, MlflowScoreLogger]
+    ],
+    experiment_id: str,
+    run_id: str,
+    ls_score_names: List[str],
+    ls_scores: List[float],
+):
+    adapter, logger = score_logger_factory(experiment_id, run_id)
+    ls_logged = []
+    mock_get_ls_score_history = mocker.patch.object(
+        adapter, "get_ls_score_history", return_value=ls_logged
+    )
+    adapter_log_spy = mocker.spy(adapter, "log_score")
+    for idx, (score_name, score) in enumerate(zip(ls_score_names, ls_scores), start=1):
+        expected_get_call_count = idx
+        expected_log_call_count = idx
+        expected_backend_path = os.path.join("artifacts", "scores", score_name)
+        expected_step = len(ls_logged) + 1
+        logger.log(artifact_name=score_name, artifact=score)
+        assert mock_get_ls_score_history.call_count == expected_get_call_count
+        mock_get_ls_score_history.assert_any_call(backend_path=expected_backend_path)
+        assert adapter_log_spy.call_count == expected_log_call_count
+        adapter_log_spy.assert_any_call(
+            backend_path=expected_backend_path, value=score, step=expected_step
+        )
+        ls_logged.append(score)
