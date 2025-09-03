9e21c14f180f85f85efcde57bfe71bfb1cda90d6
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-09-03 19:19:39 +0300

    Fix erroneous coupling of binary classification registries to mismatching base

--- artifact-core/artifact_core/binary_classification/registries/base.py
+++ artifact-core/artifact_core/binary_classification/registries/base.py
@@ -6,8 +6,8 @@ from numpy import ndarray
 
 from artifact_core.base.artifact_dependencies import ArtifactResult
 from artifact_core.base.registry import ArtifactType
-from artifact_core.core.dataset_comparison.registry import (
-    DatasetComparisonArtifactRegistry,
+from artifact_core.core.classification.registry import (
+    ClassificationArtifactRegistry,
 )
 from artifact_core.libs.resource_spec.labels.protocol import LabelsSpecProtocol
 
@@ -15,7 +15,7 @@ ArtifactTypeT = TypeVar("ArtifactTypeT", bound="ArtifactType")
 ArtifactResultT = TypeVar("ArtifactResultT", bound=ArtifactResult)
 
 
-BinaryClassificationArtifactRegistry = DatasetComparisonArtifactRegistry[
+BinaryClassificationArtifactRegistry = ClassificationArtifactRegistry[
     ArtifactTypeT,
     pd.DataFrame,
     ArtifactResultT,
