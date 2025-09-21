41ea2bfd14ead4a10662a1373026aafca4ad19ee
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-09-21 15:05:24 +0300

    Rebrand true false to positive negative

--- artifact-core/artifact_core/libs/resource_spec/binary/spec.py
+++ artifact-core/artifact_core/libs/resource_spec/binary/spec.py
@@ -4,44 +4,48 @@ from artifact_core.libs.resource_spec.categorical.spec import CategoricalFeature
 
 
 class BinaryFeatureSpec(CategoricalFeatureSpec):
-    def __init__(self, feature_name: str, true_category: str, ls_categories: List[str]):
+    def __init__(self, feature_name: str, positive_category: str, ls_categories: List[str]):
         self._assert_binary(ls_categories=ls_categories)
-        self._validate_true_category(true_category=true_category, ls_categories=ls_categories)
+        self._validate_positive_category(
+            positive_category=positive_category, ls_categories=ls_categories
+        )
         super().__init__(feature_name=feature_name, ls_categories=ls_categories)
-        self._true_category = true_category
-        self._false_category = self._get_false_category(
-            true_category=true_category, ls_categories=ls_categories
+        self._positive_category = positive_category
+        self._negative_category = self._get_negative_category(
+            positive_category=positive_category, ls_categories=ls_categories
         )
 
     @property
-    def true_category(self) -> str:
-        return self._true_category
+    def positive_category(self) -> str:
+        return self._positive_category
 
     @property
-    def false_category(self) -> str:
-        return self._false_category
+    def negative_category(self) -> str:
+        return self._negative_category
 
     def __repr__(self) -> str:
         return (
             "BinaryFeatureSpec("
             f"feature_name={self.feature_name!r}, "
             f"categories={self.ls_categories}, "
-            f"true_category={self._true_category!r}, "
-            f"false_category={self._false_category!r})"
+            f"positive_category={self._positive_category!r}, "
+            f"negative_category={self._negative_category!r})"
         )
 
     def is_positive(self, category: str) -> bool:
         self._require_category(category)
-        return category == self._true_category
+        return category == self._positive_category
 
     def is_negative(self, category: str) -> bool:
         self._require_category(category)
-        return category == self._false_category
+        return category == self._negative_category
 
     @staticmethod
-    def _get_false_category(true_category: str, ls_categories: List[str]) -> str:
-        false_category = ls_categories[0] if ls_categories[1] == true_category else ls_categories[1]
-        return false_category
+    def _get_negative_category(positive_category: str, ls_categories: List[str]) -> str:
+        negative_category = (
+            ls_categories[0] if ls_categories[1] == positive_category else ls_categories[1]
+        )
+        return negative_category
 
     @staticmethod
     def _assert_binary(ls_categories: List[str]) -> None:
@@ -55,8 +59,8 @@ class BinaryFeatureSpec(CategoricalFeatureSpec):
             )
 
     @staticmethod
-    def _validate_true_category(true_category: str, ls_categories: List[str]):
-        if true_category not in ls_categories:
+    def _validate_positive_category(positive_category: str, ls_categories: List[str]):
+        if positive_category not in ls_categories:
             raise ValueError(
-                f"`true_category` {true_category!r} must be one of ls_categories={ls_categories}."
+                f"`positive_category` {positive_category!r} must be one of ls_categories={ls_categories}."
             )
