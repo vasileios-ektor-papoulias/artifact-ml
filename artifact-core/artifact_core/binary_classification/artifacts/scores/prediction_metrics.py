c54305ee7e240dec2853bd16fbd85968dcda2c7b
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-09-21 16:29:41 +0300

    Implement and register prediciton metric artifacts

---
+++ artifact-core/artifact_core/binary_classification/artifacts/scores/prediction_metrics.py
@@ -0,0 +1,190 @@
+from artifact_core.base.artifact_dependencies import NoArtifactHyperparams
+from artifact_core.binary_classification.artifacts.base import BinaryClassificationScore
+from artifact_core.binary_classification.registries.scores.registry import (
+    BinaryClassificationScoreRegistry,
+)
+from artifact_core.binary_classification.registries.scores.types import (
+    BinaryClassificationScoreType,
+)
+from artifact_core.libs.implementation.binary_classification.prediction_metrics.calculator import (
+    BinaryPredictionMetric,
+    BinaryPredictionMetricCalculator,
+)
+from artifact_core.libs.resources.classification.binary_classification_results import (
+    BinaryClassificationResults,
+)
+from artifact_core.libs.resources.classification.binary_true_category_store import (
+    BinaryTrueCategoryStore,
+)
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.ACCURACY)
+class AccuracyScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.ACCURACY,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(
+    BinaryClassificationScoreType.BALANCED_ACCURACY
+)
+class BalancedAccuracyScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.BALANCED_ACCURACY,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.PRECISION)
+class PrecisionScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.PRECISION,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.NPV)
+class NPVScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.NPV,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.RECALL)
+class RecallScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.RECALL,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.TNR)
+class TNRScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.TNR,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.FPR)
+class FPRScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.FPR,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.FNR)
+class FNRScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.FNR,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.F1)
+class F1ScoreScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.F1,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
+
+
+@BinaryClassificationScoreRegistry.register_artifact(BinaryClassificationScoreType.MCC)
+class MCCScore(BinaryClassificationScore[NoArtifactHyperparams]):
+    def _evaluate_binary_classification(
+        self,
+        true_category_store: BinaryTrueCategoryStore,
+        classification_results: BinaryClassificationResults,
+    ) -> float:
+        result = BinaryPredictionMetricCalculator.compute(
+            metric=BinaryPredictionMetric.MCC,
+            true=true_category_store.id_to_category,
+            predicted=classification_results.prediction_store.id_to_category,
+            pos_label=self._resource_spec.positive_category,
+            neg_label=self._resource_spec.negative_category,
+        )
+        return result
