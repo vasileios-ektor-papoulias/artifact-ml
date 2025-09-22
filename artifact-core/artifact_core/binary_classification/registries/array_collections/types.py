3880ef189ef7c61b20f94f0ed1b2ec51ccd3a749
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-09-22 13:45:56 +0300

    Reorganize dirs

--- artifact-core/artifact_core/binary_classification/registries/array_collections/types.py
+++ artifact-core/artifact_core/binary_classification/registries/array_collections/types.py
@@ -2,4 +2,4 @@ from artifact_core.base.registry import ArtifactType
 
 
 class BinaryClassificationArrayCollectionType(ArtifactType):
-    pass
+    CONFUSION_MATRIX_COLLECTION = "confusion_matrix_collection"
