d0f8b24e31d6e3bcfda836d546830904936d2355
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-28 22:22:24 +0200

    Adapt experiment abstraction to ne interface

---
+++ artifact-torch/artifact_torch/binary_classification/experiment.py
@@ -0,0 +1,147 @@
+from abc import abstractmethod
+from typing import Any, Generic, Mapping, Optional, Type, TypeVar
+
+from artifact_core.libs.resource_spec.binary.protocol import BinaryFeatureSpecProtocol
+from artifact_experiment.base.data_split import DataSplit
+from artifact_experiment.base.tracking.client import TrackingClient
+
+from artifact_torch.base.components.routines.batch import BatchRoutine
+from artifact_torch.base.components.routines.data_loader import DataLoaderRoutine
+from artifact_torch.base.data.data_loader import DataLoader
+from artifact_torch.base.experiment.experiment import Experiment
+from artifact_torch.base.model.io import ModelInput, ModelOutput
+from artifact_torch.base.trainer.trainer import Trainer
+from artifact_torch.binary_classification.model import BinaryClassifier
+from artifact_torch.binary_classification.routine import (
+    BinaryClassificationRoutine,
+    BinaryClassificationRoutineData,
+)
+from artifact_torch.core.model.classifier import ClassificationParams
+
+ModelInputT = TypeVar("ModelInputT", bound=ModelInput)
+ModelOutputT = TypeVar("ModelOutputT", bound=ModelOutput)
+BinaryClassifierT = TypeVar("BinaryClassifierT", bound=BinaryClassifier[Any, Any, Any, Any])
+ClassificationParamsT = TypeVar("ClassificationParamsT", bound=ClassificationParams)
+ClassificationDataT = TypeVar("ClassificationDataT")
+GenericBinaryClassificationExperimentT = TypeVar(
+    "GenericBinaryClassificationExperimentT", bound="GenericBinaryClassificationExperiment"
+)
+
+
+class GenericBinaryClassificationExperiment(
+    Experiment[
+        BinaryClassifierT,
+        ModelInputT,
+        ModelOutputT,
+        BinaryFeatureSpecProtocol,
+        BinaryClassificationRoutineData[ClassificationDataT],
+    ],
+    Generic[
+        BinaryClassifierT, ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT
+    ],
+):
+    @classmethod
+    @abstractmethod
+    def _get_trainer_type(
+        cls,
+    ) -> Type[
+        Trainer[
+            BinaryClassifierT,
+            ModelInputT,
+            ModelOutputT,
+            Any,
+            Any,
+        ]
+    ]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_batch_routine(
+        cls,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[
+        BatchRoutine[
+            ModelInputT,
+            ModelOutputT,
+            BinaryClassifierT,
+        ]
+    ]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_loader_routine(
+        cls,
+        data_loader: DataLoader[ModelInputT],
+        data_split: DataSplit,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[DataLoaderRoutine[ModelInputT, ModelOutputT]]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_artifact_routine(
+        cls,
+        data: Mapping[DataSplit, BinaryClassificationRoutineData[ClassificationDataT]],
+        data_spec: BinaryFeatureSpecProtocol,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[BinaryClassificationRoutine[ClassificationParamsT, ClassificationDataT]]: ...
+
+
+BinaryClassificationExperimentT = TypeVar(
+    "BinaryClassificationExperimentT", bound="BinaryClassificationExperiment"
+)
+
+
+class BinaryClassificationExperiment(
+    GenericBinaryClassificationExperiment[
+        BinaryClassifier[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+        ModelInputT,
+        ModelOutputT,
+        ClassificationParamsT,
+        ClassificationDataT,
+    ],
+    Generic[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+):
+    @classmethod
+    @abstractmethod
+    def _get_trainer_type(
+        cls,
+    ) -> Type[
+        Trainer[
+            BinaryClassifier[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+            ModelInputT,
+            ModelOutputT,
+            Any,
+            Any,
+        ]
+    ]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_batch_routine(
+        cls,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[
+        BatchRoutine[
+            ModelInputT,
+            ModelOutputT,
+            BinaryClassifier[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+        ]
+    ]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_loader_routine(
+        cls,
+        data_loader: DataLoader[ModelInputT],
+        data_split: DataSplit,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[DataLoaderRoutine[ModelInputT, ModelOutputT]]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_artifact_routine(
+        cls,
+        data: Mapping[DataSplit, BinaryClassificationRoutineData[ClassificationDataT]],
+        data_spec: BinaryFeatureSpecProtocol,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[BinaryClassificationRoutine[ClassificationParamsT, ClassificationDataT]]: ...
