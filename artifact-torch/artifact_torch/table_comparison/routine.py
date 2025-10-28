fe58d8df3037d9b86dd37eb3d309c9489080e6c6
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-28 22:05:38 +0200

    Adapt table comparison routine to new pattern

--- artifact-torch/artifact_torch/table_comparison/routine.py
+++ artifact-torch/artifact_torch/table_comparison/routine.py
@@ -1,29 +1,30 @@
 from abc import abstractmethod
 from dataclasses import dataclass
-from typing import Any, Generic, Optional, Type, TypeVar
+from typing import Any, Generic, Mapping, Optional, TypeVar
 
 import pandas as pd
 from artifact_core.libs.resource_spec.tabular.protocol import TabularDataSpecProtocol
 from artifact_core.table_comparison.artifacts.base import TableComparisonArtifactResources
+from artifact_experiment.base.data_split import DataSplit
 from artifact_experiment.base.tracking.client import TrackingClient
 from artifact_experiment.table_comparison.validation_plan import TableComparisonPlan
 
 from artifact_torch.base.components.routines.artifact import (
+    ArtifactRoutine,
     ArtifactRoutineData,
     ArtifactRoutineHyperparams,
-    ArtifactValidationRoutine,
 )
 from artifact_torch.core.model.generative import GenerationParams
 from artifact_torch.libs.exports.table import TableExporter
 from artifact_torch.table_comparison.model import TableSynthesizer
 
-GenerationParamsT = TypeVar("GenerationParamsT", bound=GenerationParams)
+GenerationParamsTCov = TypeVar("GenerationParamsTCov", bound=GenerationParams, covariant=True)
 TableComparisonRoutineT = TypeVar("TableComparisonRoutineT", bound="TableComparisonRoutine")
 
 
 @dataclass
-class TableComparisonRoutineHyperparams(ArtifactRoutineHyperparams, Generic[GenerationParamsT]):
-    generation_params: GenerationParamsT
+class TableComparisonRoutineHyperparams(ArtifactRoutineHyperparams, Generic[GenerationParamsTCov]):
+    generation_params: GenerationParamsTCov
 
 
 @dataclass
@@ -32,79 +33,67 @@ class TableComparisonRoutineData(ArtifactRoutineData):
 
 
 class TableComparisonRoutine(
-    ArtifactValidationRoutine[
-        TableSynthesizer[Any, Any, GenerationParamsT],
-        TableComparisonRoutineHyperparams[GenerationParamsT],
+    ArtifactRoutine[
+        TableSynthesizer[Any, Any, GenerationParamsTCov],
+        TableComparisonRoutineHyperparams[GenerationParamsTCov],
         TableComparisonRoutineData,
         TableComparisonArtifactResources,
         TabularDataSpecProtocol,
     ],
-    Generic[GenerationParamsT],
+    Generic[GenerationParamsTCov],
 ):
-    _resource_export_prefix = "synthetic"
-
-    @classmethod
-    def build(
-        cls: Type[TableComparisonRoutineT],
-        df_real: pd.DataFrame,
-        data_spec: TabularDataSpecProtocol,
-        tracking_client: Optional[TrackingClient] = None,
-    ) -> TableComparisonRoutineT:
-        data = TableComparisonRoutineData(df_real=df_real)
-        routine = cls._build(
-            data=data,
-            artifact_resource_spec=data_spec,
-            tracking_client=tracking_client,
-        )
-        return routine
+    _resource_export_prefix = "SYNTHETIC"
 
     @classmethod
     @abstractmethod
-    def _get_period(cls) -> int: ...
+    def _get_periods(cls) -> Mapping[DataSplit, int]: ...
 
     @classmethod
     @abstractmethod
-    def _get_generation_params(cls) -> GenerationParamsT: ...
-
-    @classmethod
-    @abstractmethod
-    def _get_validation_plan(
+    def _get_validation_plans(
         cls,
         artifact_resource_spec: TabularDataSpecProtocol,
         tracking_client: Optional[TrackingClient],
-    ) -> TableComparisonPlan: ...
+    ) -> Mapping[DataSplit, TableComparisonPlan]: ...
 
     @classmethod
-    def _get_hyperparams(cls) -> TableComparisonRoutineHyperparams[GenerationParamsT]:
+    @abstractmethod
+    def _get_generation_params(cls) -> GenerationParamsTCov: ...
+
+    @classmethod
+    def _get_hyperparams(cls) -> TableComparisonRoutineHyperparams[GenerationParamsTCov]:
         generation_params = cls._get_generation_params()
-        hyperparams = TableComparisonRoutineHyperparams[GenerationParamsT](
+        hyperparams = TableComparisonRoutineHyperparams[GenerationParamsTCov](
             generation_params=generation_params
         )
         return hyperparams
 
-    @classmethod
     def _generate_artifact_resources(
-        cls,
-        model: TableSynthesizer[Any, Any, GenerationParamsT],
-        hyperparams: TableComparisonRoutineHyperparams,
-        data: TableComparisonRoutineData,
-    ) -> TableComparisonArtifactResources:
-        df_synthetic = model.generate(params=hyperparams.generation_params)
-        resources = TableComparisonArtifactResources(
-            dataset_real=data.df_real, dataset_synthetic=df_synthetic
-        )
-        return resources
+        self,
+        model: TableSynthesizer[Any, Any, GenerationParamsTCov],
+    ) -> Mapping[DataSplit, TableComparisonArtifactResources]:
+        df_synthetic = model.generate(params=self._hyperparams.generation_params)
+        resources_by_split = {}
+        for data_split in self._data.keys():
+            resources = TableComparisonArtifactResources(
+                dataset_real=self._data[data_split].df_real, dataset_synthetic=df_synthetic
+            )
+            resources_by_split[data_split] = resources
+        return resources_by_split
 
     @classmethod
     def _export_artifact_resources(
         cls,
         artifact_resources: TableComparisonArtifactResources,
         n_epochs_elapsed: int,
+        data_split: DataSplit,
         tracking_client: TrackingClient,
     ):
+        _ = data_split
+        prefix = cls._resource_export_prefix
         TableExporter.export(
             data=artifact_resources.dataset_synthetic,
             tracking_client=tracking_client,
-            prefix=cls._resource_export_prefix,
+            prefix=prefix,
             step=n_epochs_elapsed,
         )
