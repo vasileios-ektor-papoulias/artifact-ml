ad24a5c30db834929f3a9e6b8bd7dc7a3bb0b75c
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-28 03:00:24 +0200

    Implement data split suffix appender for callback keys

--- artifact-experiment/artifact_experiment/base/data_split.py
+++ artifact-experiment/artifact_experiment/base/data_split.py
@@ -5,3 +5,16 @@ class DataSplit(Enum):
     TRAIN = "TRAIN"
     VALIDATION = "VALIDATION"
     TEST = "TEST"
+
+
+class DataSplitSuffixAppender:
+    @staticmethod
+    def append_suffix(name: str, data_split: DataSplit) -> str:
+        if data_split is DataSplit.TRAIN:
+            return f"{name}_train"
+        elif data_split is DataSplit.VALIDATION:
+            return f"{name}_val"
+        elif data_split is DataSplit.TEST:
+            return f"{name}_test"
+        else:
+            raise ValueError(f"Unrecognized DataSplit value: {data_split}")
