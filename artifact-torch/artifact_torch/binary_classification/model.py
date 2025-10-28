295e974f7306a618e4c85691edc79e6b557da5ac
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-28 22:22:41 +0200

    Reorder generics

--- artifact-torch/artifact_torch/binary_classification/model.py
+++ artifact-torch/artifact_torch/binary_classification/model.py
@@ -1,7 +1,6 @@
 from abc import abstractmethod
 from typing import Generic, TypeVar
 
-import pandas as pd
 from artifact_core.libs.resources.classification.binary_classification_results import (
     BinaryClassificationResults,
 )
@@ -17,23 +16,26 @@ from artifact_torch.core.model.classifier import (
 
 ModelInputTContr = TypeVar("ModelInputTContr", bound=ModelInput, contravariant=True)
 ModelOutputTCov = TypeVar("ModelOutputTCov", bound=ModelOutput, covariant=True)
-ClassificationParamsT = TypeVar("ClassificationParamsT", bound=ClassificationParams)
+ClassificationParamsTContr = TypeVar(
+    "ClassificationParamsTContr", bound=ClassificationParams, contravariant=True
+)
+ClassificationDataT = TypeVar("ClassificationDataT")
 
 
 class BinaryClassifier(
     Classifier[
         ModelInputTContr,
         ModelOutputTCov,
-        ClassificationParamsT,
-        pd.DataFrame,
+        ClassificationParamsTContr,
         BinaryClassificationResults,
+        ClassificationDataT,
     ],
-    Generic[ModelInputTContr, ModelOutputTCov, ClassificationParamsT],
+    Generic[ModelInputTContr, ModelOutputTCov, ClassificationParamsTContr, ClassificationDataT],
 ):
     @abstractmethod
     def forward(self, model_input: ModelInputTContr, *args, **kwargs) -> ModelOutputTCov: ...
 
     @abstractmethod
     def classify(
-        self, data: pd.DataFrame, params: ClassificationParamsT
+        self, data: ClassificationDataT, params: ClassificationParamsTContr
     ) -> BinaryClassificationResults: ...
