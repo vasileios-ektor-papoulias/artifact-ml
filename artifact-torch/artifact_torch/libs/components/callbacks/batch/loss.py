210e5c45ac2faaa218b64046ca248471a2d73130
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-28 03:16:43 +0200

    Lowercase key

--- artifact-torch/artifact_torch/libs/components/callbacks/batch/loss.py
+++ artifact-torch/artifact_torch/libs/components/callbacks/batch/loss.py
@@ -2,18 +2,16 @@ from artifact_torch.base.components.callbacks.batch import (
     BatchScoreCallback,
 )
 from artifact_torch.base.model.base import Model
-from artifact_torch.base.model.io import ModelInput, ModelOutput
+from artifact_torch.base.model.io import LossOutput, ModelInput
 
 
-class BatchLossCallback(BatchScoreCallback[ModelInput, ModelOutput, Model]):
+class BatchLossCallback(BatchScoreCallback[ModelInput, LossOutput, Model]):
     @classmethod
     def _get_key(cls):
-        return "BATCH_LOSS"
+        return "batch_loss"
 
     @staticmethod
-    def _compute_on_batch(
-        model_input: ModelInput, model_output: ModelOutput, model: Model
-    ) -> float:
+    def _compute_on_batch(model_input: ModelInput, model_output: LossOutput, model: Model) -> float:
         _ = model_input
         _ = model
         t_loss = model_output.get("t_loss")
