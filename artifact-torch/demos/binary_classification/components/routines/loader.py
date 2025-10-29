2148340a00e9073f840121de666b511c8f33c267
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-30 01:39:16 +0200

    Fix type errors that appeared after refactor

--- artifact-torch/demos/binary_classification/components/routines/loader.py
+++ artifact-torch/demos/binary_classification/components/routines/loader.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from artifact_torch.base.components.callbacks.data_loader import (
+from artifact_experiment.base.data_split import DataSplit
+from artifact_torch.base.components.callbacks.loader import (
     DataLoaderArrayCallback,
     DataLoaderArrayCollectionCallback,
     DataLoaderPlotCallback,
@@ -8,8 +9,8 @@ from artifact_torch.base.components.callbacks.data_loader import (
     DataLoaderScoreCallback,
     DataLoaderScoreCollectionCallback,
 )
-from artifact_torch.base.components.routines.data_loader import DataLoaderRoutine
-from artifact_torch.libs.components.callbacks.data_loader.loss import TrainLossCallback
+from artifact_torch.base.components.routines.loader import DataLoaderRoutine
+from artifact_torch.libs.components.callbacks.loader.loss import LoaderLossCallback
 
 from demos.binary_classification.config.constants import TRAIN_LOADER_CALLBACK_PERIOD
 from demos.binary_classification.model.io import MLPClassifierInput, MLPClassifierOutput
@@ -17,37 +18,42 @@ from demos.binary_classification.model.io import MLPClassifierInput, MLPClassifi
 
 class DemoLoaderRoutine(DataLoaderRoutine[MLPClassifierInput, MLPClassifierOutput]):
     @staticmethod
-    def _get_score_callbacks() -> List[
-        DataLoaderScoreCallback[MLPClassifierInput, MLPClassifierOutput]
-    ]:
-        return [TrainLossCallback(period=TRAIN_LOADER_CALLBACK_PERIOD)]
+    def _get_score_callbacks(
+        data_split: DataSplit,
+    ) -> List[DataLoaderScoreCallback[MLPClassifierInput, MLPClassifierOutput]]:
+        return [LoaderLossCallback(period=TRAIN_LOADER_CALLBACK_PERIOD, data_split=data_split)]
 
     @staticmethod
-    def _get_array_callbacks() -> List[
-        DataLoaderArrayCallback[MLPClassifierInput, MLPClassifierOutput]
-    ]:
+    def _get_array_callbacks(
+        data_split: DataSplit,
+    ) -> List[DataLoaderArrayCallback[MLPClassifierInput, MLPClassifierOutput]]:
+        _ = data_split
         return []
 
     @staticmethod
-    def _get_plot_callbacks() -> List[
-        DataLoaderPlotCallback[MLPClassifierInput, MLPClassifierOutput]
-    ]:
+    def _get_plot_callbacks(
+        data_split: DataSplit,
+    ) -> List[DataLoaderPlotCallback[MLPClassifierInput, MLPClassifierOutput]]:
+        _ = data_split
         return []
 
     @staticmethod
-    def _get_score_collection_callbacks() -> List[
-        DataLoaderScoreCollectionCallback[MLPClassifierInput, MLPClassifierOutput]
-    ]:
+    def _get_score_collection_callbacks(
+        data_split: DataSplit,
+    ) -> List[DataLoaderScoreCollectionCallback[MLPClassifierInput, MLPClassifierOutput]]:
+        _ = data_split
         return []
 
     @staticmethod
-    def _get_array_collection_callbacks() -> List[
-        DataLoaderArrayCollectionCallback[MLPClassifierInput, MLPClassifierOutput]
-    ]:
+    def _get_array_collection_callbacks(
+        data_split: DataSplit,
+    ) -> List[DataLoaderArrayCollectionCallback[MLPClassifierInput, MLPClassifierOutput]]:
+        _ = data_split
         return []
 
     @staticmethod
-    def _get_plot_collection_callbacks() -> List[
-        DataLoaderPlotCollectionCallback[MLPClassifierInput, MLPClassifierOutput]
-    ]:
+    def _get_plot_collection_callbacks(
+        data_split: DataSplit,
+    ) -> List[DataLoaderPlotCollectionCallback[MLPClassifierInput, MLPClassifierOutput]]:
+        _ = data_split
         return []
