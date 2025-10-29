ea77927afb911ef64fd45597b26863e1b82cd824
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-10-29 11:45:30 +0200

    Capitalize for consistency with other logging style

--- artifact-experiment/artifact_experiment/base/data_split.py
+++ artifact-experiment/artifact_experiment/base/data_split.py
@@ -11,10 +11,10 @@ class DataSplitSuffixAppender:
     @staticmethod
     def append_suffix(name: str, data_split: DataSplit) -> str:
         if data_split is DataSplit.TRAIN:
-            return f"{name}_train"
+            return f"{name}_TRAIN"
         elif data_split is DataSplit.VALIDATION:
-            return f"{name}_val"
+            return f"{name}_VAL"
         elif data_split is DataSplit.TEST:
-            return f"{name}_test"
+            return f"{name}_TEST"
         else:
             raise ValueError(f"Unrecognized DataSplit value: {data_split}")
