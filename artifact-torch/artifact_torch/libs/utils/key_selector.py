9f2e777cf77ee9021706251f90338b30faba46b1
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-01 02:19:06 +0200

    Add utils to key selector

--- artifact-torch/artifact_torch/libs/utils/key_selector.py
+++ artifact-torch/artifact_torch/libs/utils/key_selector.py
@@ -5,5 +5,16 @@ HashableT = TypeVar("HashableT", bound=Hashable)
 
 class KeySelector:
     @staticmethod
+    def get_non_null_keys(d: Mapping[HashableT, Any]) -> Set[HashableT]:
+        return {k for k, v in d.items() if v is not None}
+
+    @staticmethod
+    def get_common_non_null_keys(*dicts: Mapping[HashableT, Any]) -> Set[HashableT]:
+        if not dicts:
+            return set()
+        common = set(dicts[0]).intersection(*dicts[1:])
+        return {k for k in common if all(d.get(k) is not None for d in dicts)}
+
+    @staticmethod
     def get_common_keys(*dicts: Mapping[HashableT, Any]) -> Set[HashableT]:
         return set(dicts[0]).intersection(*dicts[1:]) if dicts else set()
