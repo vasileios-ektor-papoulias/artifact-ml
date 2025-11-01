6a0c416d9f0b73a2727282a6e9622053eb55adaa
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-01 02:17:41 +0200

    Adapt binary classification experiment

--- artifact-torch/artifact_torch/binary_classification/experiment.py
+++ artifact-torch/artifact_torch/binary_classification/experiment.py
@@ -7,7 +7,7 @@ from artifact_experiment.base.tracking.client import TrackingClient
 
 from artifact_torch.base.components.routines.batch import BatchRoutine
 from artifact_torch.base.components.routines.loader import DataLoaderRoutine
-from artifact_torch.base.data.data_loader import DataLoader
+from artifact_torch.base.components.routines.loader_hook import DataLoaderHookRoutine
 from artifact_torch.base.experiment.experiment import Experiment
 from artifact_torch.base.model.io import ModelInput, ModelOutput
 from artifact_torch.base.trainer.trainer import Trainer
@@ -18,9 +18,9 @@ from artifact_torch.binary_classification.routine import (
 )
 from artifact_torch.core.model.classifier import ClassificationParams
 
+BinaryClassifierT = TypeVar("BinaryClassifierT", bound=BinaryClassifier[Any, Any, Any, Any])
 ModelInputT = TypeVar("ModelInputT", bound=ModelInput)
 ModelOutputT = TypeVar("ModelOutputT", bound=ModelOutput)
-BinaryClassifierT = TypeVar("BinaryClassifierT", bound=BinaryClassifier[Any, Any, Any, Any])
 ClassificationParamsT = TypeVar("ClassificationParamsT", bound=ClassificationParams)
 ClassificationDataT = TypeVar("ClassificationDataT")
 BinaryClassificationExperimentT = TypeVar(
@@ -30,13 +30,15 @@ BinaryClassificationExperimentT = TypeVar(
 
 class BinaryClassificationExperiment(
     Experiment[
-        BinaryClassifier[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+        BinaryClassifierT,
         ModelInputT,
         ModelOutputT,
         BinaryClassificationRoutineData[ClassificationDataT],
         BinaryFeatureSpecProtocol,
     ],
-    Generic[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+    Generic[
+        BinaryClassifierT, ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT
+    ],
 ):
     @classmethod
     @abstractmethod
@@ -44,7 +46,7 @@ class BinaryClassificationExperiment(
         cls,
     ) -> Type[
         Trainer[
-            BinaryClassifier[ModelInputT, ModelOutputT, ClassificationParamsT, ClassificationDataT],
+            BinaryClassifierT,
             ModelInputT,
             ModelOutputT,
             Any,
@@ -63,10 +65,17 @@ class BinaryClassificationExperiment(
     @abstractmethod
     def _get_loader_routine(
         cls,
-        data_loader: DataLoader[ModelInputT],
         data_split: DataSplit,
         tracking_client: Optional[TrackingClient] = None,
     ) -> Optional[DataLoaderRoutine[ModelInputT, ModelOutputT]]: ...
+
+    @classmethod
+    @abstractmethod
+    def _get_loader_hook_routine(
+        cls,
+        data_split: DataSplit,
+        tracking_client: Optional[TrackingClient] = None,
+    ) -> Optional[DataLoaderHookRoutine[BinaryClassifierT, ModelInputT, ModelOutputT]]: ...
 
     @classmethod
     @abstractmethod
