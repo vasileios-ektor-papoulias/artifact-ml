1869e6454e71d0d98f0a928bb51590d208ad4c22
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-01 02:15:15 +0200

    Adapt epoch end flow

--- artifact-torch/artifact_torch/base/trainer/epoch_end_flow.py
+++ artifact-torch/artifact_torch/base/trainer/epoch_end_flow.py
@@ -1,10 +1,11 @@
-from typing import Any, Dict, Generic, Optional, Sequence, TypeVar
+from typing import Any, Dict, Generic, Mapping, Optional, TypeVar
 
+from artifact_experiment.base.data_split import DataSplit
 from matplotlib.figure import Figure
 from numpy import ndarray
 
 from artifact_torch.base.components.routines.artifact import ArtifactRoutine
-from artifact_torch.base.components.routines.loader import DataLoaderRoutine
+from artifact_torch.base.components.routines.joint_loader import JointDataLoaderRoutine
 from artifact_torch.base.model.base import Model
 from artifact_torch.base.model.io import ModelInput, ModelOutput
 
@@ -18,12 +19,14 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         self,
         artifact_routine: Optional[ArtifactRoutine[ModelTContr, Any, Any, Any, Any]] = None,
         loader_routines: Optional[
-            Sequence[DataLoaderRoutine[ModelInputTContr, ModelOutputTContr]]
+            Mapping[
+                DataSplit, JointDataLoaderRoutine[ModelTContr, ModelInputTContr, ModelOutputTContr]
+            ]
         ] = None,
     ):
-        self._artifact_routine = artifact_routine
         if loader_routines is None:
-            loader_routines = []
+            loader_routines = {}
+        self._artifact_routine = artifact_routine
         self._loader_routines = loader_routines
 
     @property
@@ -31,7 +34,7 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         scores = {}
         if self._artifact_routine is not None:
             scores.update(self._artifact_routine.scores)
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             scores.update(loader_routine.scores)
         return scores
 
@@ -40,7 +43,7 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         arrays = {}
         if self._artifact_routine is not None:
             arrays.update(self._artifact_routine.arrays)
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             arrays.update(loader_routine.arrays)
         return arrays
 
@@ -49,7 +52,7 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         plots = {}
         if self._artifact_routine is not None:
             plots.update(self._artifact_routine.plots)
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             plots.update(loader_routine.plots)
         return plots
 
@@ -58,7 +61,7 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         score_collections = {}
         if self._artifact_routine is not None:
             score_collections.update(self._artifact_routine.score_collections)
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             score_collections.update(loader_routine.score_collections)
         return score_collections
 
@@ -67,7 +70,7 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         array_collections = {}
         if self._artifact_routine is not None:
             array_collections.update(self._artifact_routine.array_collections)
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             array_collections.update(loader_routine.array_collections)
         return array_collections
 
@@ -76,18 +79,18 @@ class EpochEndFlow(Generic[ModelTContr, ModelInputTContr, ModelOutputTContr]):
         plot_collections = {}
         if self._artifact_routine is not None:
             plot_collections.update(self._artifact_routine.plot_collections)
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             plot_collections.update(loader_routine.plot_collections)
         return plot_collections
-
-    def execute(self, model: ModelTContr, n_epochs_elapsed: int):
-        if self._artifact_routine is not None:
-            self._artifact_routine.execute(model=model, n_epochs_elapsed=n_epochs_elapsed)
-        for loader_routine in self._loader_routines:
-            loader_routine.execute(model=model, n_epochs_elapsed=n_epochs_elapsed)
 
     def clear_cache(self):
         if self._artifact_routine is not None:
             self._artifact_routine.clear_cache()
-        for loader_routine in self._loader_routines:
+        for loader_routine in self._loader_routines.values():
             loader_routine.clear_cache()
+
+    def execute(self, model: ModelTContr, n_epochs_elapsed: int):
+        if self._artifact_routine is not None:
+            self._artifact_routine.execute(model=model, n_epochs_elapsed=n_epochs_elapsed)
+        for loader_routine in self._loader_routines.values():
+            loader_routine.execute(model=model, n_epochs_elapsed=n_epochs_elapsed)
