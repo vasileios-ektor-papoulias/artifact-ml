7cd3ca1089f16b8d990739d78b6af820645fa7f5
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-03 03:03:31 +0200

    Perform cosmetic changes for clarity

--- artifact-torch/artifact_torch/base/components/routines/artifact.py
+++ artifact-torch/artifact_torch/base/components/routines/artifact.py
@@ -4,10 +4,9 @@ from typing import Any, Dict, Generic, Mapping, Optional, Set, Type, TypeVar
 
 from artifact_core.base.artifact import ResourceSpecProtocol
 from artifact_core.base.artifact_dependencies import ArtifactResources
-from artifact_experiment.base.callbacks.artifact import ArtifactCallbackResources
-from artifact_experiment.base.data_split import DataSplit
+from artifact_experiment.base.entities.data_split import DataSplit
+from artifact_experiment.base.plans.artifact import ArtifactPlan
 from artifact_experiment.base.tracking.client import TrackingClient
-from artifact_experiment.base.validation_plan import ValidationPlan
 from matplotlib.figure import Figure
 from numpy import ndarray
 
@@ -51,26 +50,26 @@ class ArtifactRoutine(
 ):
     def __init__(
         self,
-        periods: Mapping[DataSplit, int],
-        data: Mapping[DataSplit, ArtifactRoutineDataTContr],
-        validation_plans: Mapping[
+        artifact_plans: Mapping[
             DataSplit,
-            ValidationPlan[
-                Any, Any, Any, Any, Any, Any, ArtifactResourcesTContr, ResourceSpecProtocolTContr
+            ArtifactPlan[
+                ArtifactResourcesTContr, ResourceSpecProtocolTContr, Any, Any, Any, Any, Any, Any
             ],
         ],
+        data: Mapping[DataSplit, ArtifactRoutineDataTContr],
+        periods: Mapping[DataSplit, int],
         hyperparams: ArtifactRoutineHyperparamsTCov,
         tracking_client: Optional[TrackingClient] = None,
     ):
-        self._periods = periods
+        self._artifact_plans = artifact_plans
         self._data = data
-        self._validation_plans = validation_plans
+        self._periods = periods
         self._hyperparams = hyperparams
         self._tracking_client = tracking_client
-        self._splits = KeySelector.get_common_keys(
+        self._data_splits = KeySelector.get_common_keys(
             self._periods,
             self._data,
-            self._validation_plans,
+            self._artifact_plans,
         )
 
     @classmethod
@@ -86,11 +85,11 @@ class ArtifactRoutine(
             if (period := cls._get_period(data_split=data_split)) is not None
         }
 
-        validation_plans = {
+        artifact_plans = {
             data_split: plan
             for data_split in DataSplit
             if (
-                plan := cls._get_validation_plan(
+                plan := cls._get_artifact_plan(
                     artifact_resource_spec=data_spec,
                     data_split=data_split,
                     tracking_client=tracking_client,
@@ -100,9 +99,9 @@ class ArtifactRoutine(
         }
         hyperparams = cls._get_hyperparams()
         routine = cls(
-            periods=periods,
+            artifact_plans=artifact_plans,
             data=data,
-            validation_plans=validation_plans,
+            periods=periods,
             hyperparams=hyperparams,
             tracking_client=tracking_client,
         )
@@ -112,48 +111,48 @@ class ArtifactRoutine(
     def scores(self) -> Dict[str, float]:
         return {
             name: value
-            for validation_plan in self._validation_plans.values()
-            for name, value in validation_plan.scores.items()
+            for artifact_plan in self._artifact_plans.values()
+            for name, value in artifact_plan.scores.items()
         }
 
     @property
     def arrays(self) -> Dict[str, ndarray]:
         return {
             name: value
-            for validation_plan in self._validation_plans.values()
-            for name, value in validation_plan.arrays.items()
+            for artifact_plan in self._artifact_plans.values()
+            for name, value in artifact_plan.arrays.items()
         }
 
     @property
     def plots(self) -> Dict[str, Figure]:
         return {
             name: value
-            for validation_plan in self._validation_plans.values()
-            for name, value in validation_plan.plots.items()
+            for artifact_plan in self._artifact_plans.values()
+            for name, value in artifact_plan.plots.items()
         }
 
     @property
     def score_collections(self) -> Dict[str, Dict[str, float]]:
         return {
             name: value
-            for validation_plan in self._validation_plans.values()
-            for name, value in validation_plan.score_collections.items()
+            for artifact_plan in self._artifact_plans.values()
+            for name, value in artifact_plan.score_collections.items()
         }
 
     @property
     def array_collections(self) -> Dict[str, Dict[str, ndarray]]:
         return {
             name: value
-            for validation_plan in self._validation_plans.values()
-            for name, value in validation_plan.array_collections.items()
+            for artifact_plan in self._artifact_plans.values()
+            for name, value in artifact_plan.array_collections.items()
         }
 
     @property
     def plot_collections(self) -> Dict[str, Dict[str, Figure]]:
         return {
             name: value
-            for validation_plan in self._validation_plans.values()
-            for name, value in validation_plan.plot_collections.items()
+            for artifact_plan in self._artifact_plans.values()
+            for name, value in artifact_plan.plot_collections.items()
         }
 
     @classmethod
@@ -162,14 +161,14 @@ class ArtifactRoutine(
 
     @classmethod
     @abstractmethod
-    def _get_validation_plan(
+    def _get_artifact_plan(
         cls,
         artifact_resource_spec: ResourceSpecProtocolTContr,
         data_split: DataSplit,
         tracking_client: Optional[TrackingClient],
     ) -> Optional[
-        ValidationPlan[
-            Any, Any, Any, Any, Any, Any, ArtifactResourcesTContr, ResourceSpecProtocolTContr
+        ArtifactPlan[
+            ArtifactResourcesTContr, ResourceSpecProtocolTContr, Any, Any, Any, Any, Any, Any
         ]
     ]: ...
 
@@ -194,19 +193,16 @@ class ArtifactRoutine(
     ) -> Mapping[DataSplit, ArtifactResourcesTContr]: ...
 
     def clear_cache(self):
-        for validation_plan in self._validation_plans.values():
+        for validation_plan in self._artifact_plans.values():
             validation_plan.clear_cache()
 
     def execute(self, model: ModelTContr, n_epochs_elapsed: int):
-        splits = self._get_active_splits(n_epochs_elapsed=n_epochs_elapsed)
+        data_splits = self._get_active_splits(n_epochs_elapsed=n_epochs_elapsed)
         artifact_resources_by_split = self._generate_artifact_resources(model=model)
-        for data_split in splits:
-            validation_plan = self._validation_plans[data_split]
+        for data_split in data_splits:
+            artifact_plan = self._artifact_plans[data_split]
             artifact_resources = artifact_resources_by_split[data_split]
-            artifact_callback_resources = ArtifactCallbackResources(
-                artifact_resources=artifact_resources
-            )
-            validation_plan.execute(resources=artifact_callback_resources)
+            artifact_plan.execute_artifacts(resources=artifact_resources)
             if self._tracking_client is not None:
                 self._export_artifact_resources(
                     artifact_resources=artifact_resources,
@@ -218,7 +214,7 @@ class ArtifactRoutine(
     def _get_active_splits(self, n_epochs_elapsed: int) -> Set[DataSplit]:
         return {
             split
-            for split in self._splits
+            for split in self._data_splits
             if PeriodicActionTrigger.should_trigger(
                 step=n_epochs_elapsed, period=self._periods[split]
             )
