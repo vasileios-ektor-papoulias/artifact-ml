a386d9335c2de9278315e3a2e72cf179b53ee50f
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-04 12:00:58 +0200

    Adapt bae experiment class

--- artifact-torch/artifact_torch/base/experiment/experiment.py
+++ artifact-torch/artifact_torch/base/experiment/experiment.py
@@ -6,47 +6,54 @@ from artifact_core.base.artifact import ResourceSpecProtocol
 from artifact_experiment.base.entities.data_split import DataSplit
 from artifact_experiment.base.tracking.client import TrackingClient
 
-from artifact_torch.base.components.cache.cache import StandardCache
 from artifact_torch.base.components.routines.artifact import ArtifactRoutine, ArtifactRoutineData
-from artifact_torch.base.components.routines.batch import BatchRoutine
 from artifact_torch.base.components.routines.loader import DataLoaderRoutine
+from artifact_torch.base.components.routines.train_diagnostics import TrainDiagnosticsRoutine
 from artifact_torch.base.data.data_loader import DataLoader
 from artifact_torch.base.model.base import Model
 from artifact_torch.base.model.io import ModelInput, ModelOutput
 from artifact_torch.base.trainer.trainer import Trainer
 
-ModelT = TypeVar("ModelT", bound=Model[Any, Any])
-ModelInputT = TypeVar("ModelInputT", bound=ModelInput)
-ModelOutputT = TypeVar("ModelOutputT", bound=ModelOutput)
-ArtifactRoutineDataT = TypeVar("ArtifactRoutineDataT", bound=ArtifactRoutineData)
-DataSpecProtocolT = TypeVar("DataSpecProtocolT", bound=ResourceSpecProtocol)
+ModelTContr = TypeVar("ModelTContr", bound=Model[Any, Any], contravariant=True)
+ModelInputTContr = TypeVar("ModelInputTContr", bound=ModelInput, contravariant=True)
+ModelOutputTContr = TypeVar("ModelOutputTContr", bound=ModelOutput, contravariant=True)
+ArtifactRoutineDataTContr = TypeVar(
+    "ArtifactRoutineDataTContr", bound=ArtifactRoutineData, contravariant=True
+)
+DataSpecProtocolTContr = TypeVar(
+    "DataSpecProtocolTContr", bound=ResourceSpecProtocol, contravariant=True
+)
 ExperimentT = TypeVar("ExperimentT", bound="Experiment")
 
 
 class Experiment(
     ABC,
     Generic[
-        ModelT,
-        ModelInputT,
-        ModelOutputT,
-        ArtifactRoutineDataT,
-        DataSpecProtocolT,
+        ModelTContr,
+        ModelInputTContr,
+        ModelOutputTContr,
+        ArtifactRoutineDataTContr,
+        DataSpecProtocolTContr,
     ],
 ):
-    def __init__(self, trainer: Trainer[ModelT, ModelInputT, ModelOutputT, Any, Any]):
+    def __init__(
+        self, trainer: Trainer[ModelTContr, ModelInputTContr, ModelOutputTContr, Any, Any]
+    ):
         self._trainer = trainer
 
     @classmethod
     def build(
         cls: Type[ExperimentT],
-        model: ModelT,
-        data_loaders: Mapping[DataSplit, DataLoader[ModelInputT]],
-        artifact_routine_data: Mapping[DataSplit, ArtifactRoutineDataT],
-        artifact_routine_data_spec: Optional[DataSpecProtocolT] = None,
+        model: ModelTContr,
+        data_loaders: Mapping[DataSplit, DataLoader[ModelInputTContr]],
+        artifact_routine_data: Mapping[DataSplit, ArtifactRoutineDataTContr],
+        artifact_routine_data_spec: Optional[DataSpecProtocolTContr] = None,
         tracking_client: Optional[TrackingClient] = None,
     ) -> ExperimentT:
         assert DataSplit.TRAIN in data_loaders, "Training data not provided."
-        batch_routine = cls._get_batch_routine(tracking_client=tracking_client)
+        train_diagnostics_routine = cls._get_train_diagnostics_routine(
+            tracking_client=tracking_client
+        )
         loader_routine = cls._get_loader_routine(
             data_loaders=data_loaders, tracking_client=tracking_client
         )
@@ -58,7 +65,7 @@ class Experiment(
         trainer = cls._get_trainer_type().build(
             model=model,
             train_loader=data_loaders[DataSplit.TRAIN],
-            batch_routine=batch_routine,
+            train_diagnostics_routine=train_diagnostics_routine,
             loader_routine=loader_routine,
             artifact_routine=artifact_routine,
         )
@@ -66,46 +73,38 @@ class Experiment(
         return experiment
 
     @property
-    def model(self) -> ModelT:
-        return self._trainer.model
-
-    @property
     def epoch_scores(self) -> pd.DataFrame:
         return self._trainer.epoch_scores
-
-    @property
-    def batch_cache(self) -> StandardCache[Any]:
-        return self._trainer.batch_cache
 
     @classmethod
     @abstractmethod
     def _get_trainer_type(
         cls,
-    ) -> Type[Trainer[ModelT, ModelInputT, ModelOutputT, Any, Any]]: ...
+    ) -> Type[Trainer[ModelTContr, ModelInputTContr, ModelOutputTContr, Any, Any]]: ...
 
     @classmethod
     @abstractmethod
-    def _get_batch_routine(
+    def _get_train_diagnostics_routine(
         cls,
         tracking_client: Optional[TrackingClient] = None,
-    ) -> Optional[BatchRoutine[ModelInputT, ModelOutputT]]: ...
+    ) -> Optional[TrainDiagnosticsRoutine[ModelTContr, ModelInputTContr, ModelOutputTContr]]: ...
 
     @classmethod
     @abstractmethod
     def _get_loader_routine(
         cls,
-        data_loaders: Mapping[DataSplit, DataLoader[ModelInputT]],
+        data_loaders: Mapping[DataSplit, DataLoader[ModelInputTContr]],
         tracking_client: Optional[TrackingClient] = None,
-    ) -> Optional[DataLoaderRoutine[ModelT, ModelInputT, ModelOutputT]]: ...
+    ) -> Optional[DataLoaderRoutine[ModelTContr, ModelInputTContr, ModelOutputTContr]]: ...
 
     @classmethod
     @abstractmethod
     def _get_artifact_routine(
         cls,
-        data: Mapping[DataSplit, ArtifactRoutineDataT],
-        data_spec: DataSpecProtocolT,
+        data: Mapping[DataSplit, ArtifactRoutineDataTContr],
+        data_spec: DataSpecProtocolTContr,
         tracking_client: Optional[TrackingClient] = None,
-    ) -> Optional[ArtifactRoutine[ModelT, Any, Any, Any, Any]]: ...
+    ) -> Optional[ArtifactRoutine[ModelTContr, Any, Any, Any, Any]]: ...
 
     def run(self):
         self._trainer.train()
