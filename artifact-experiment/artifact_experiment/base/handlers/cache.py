97a141cba456e845d3e603fa5ca02f8eac750403
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-05 00:03:06 +0200

    Couple cache callback handler to cache callback resources

--- artifact-experiment/artifact_experiment/base/handlers/cache.py
+++ artifact-experiment/artifact_experiment/base/handlers/cache.py
@@ -1,19 +1,18 @@
 from typing import Any, Dict, Generic, Optional, Sequence, TypeVar
 
-from artifact_experiment.base.callbacks.base import CallbackResources
-from artifact_experiment.base.callbacks.cache import CacheCallback
+from artifact_experiment.base.callbacks.cache import CacheCallback, CacheCallbackResources
 from artifact_experiment.base.handlers.base import CallbackHandler
 
 CacheCallbackTCov = TypeVar("CacheCallbackTCov", bound=CacheCallback[Any, Any], covariant=True)
-CallbackResourcesTContr = TypeVar(
-    "CallbackResourcesTContr", bound=CallbackResources, contravariant=True
+CacheCallbackResourcesTContr = TypeVar(
+    "CacheCallbackResourcesTContr", bound=CacheCallbackResources, contravariant=True
 )
 CacheDataTCov = TypeVar("CacheDataTCov", covariant=True)
 
 
 class CacheCallbackHandler(
-    CallbackHandler[CacheCallbackTCov, CallbackResourcesTContr],
-    Generic[CacheCallbackTCov, CallbackResourcesTContr, CacheDataTCov],
+    CallbackHandler[CacheCallbackTCov, CacheCallbackResourcesTContr],
+    Generic[CacheCallbackTCov, CacheCallbackResourcesTContr, CacheDataTCov],
 ):
     def __init__(self, callbacks: Optional[Sequence[CacheCallbackTCov]] = None):
         super().__init__(callbacks=callbacks)
@@ -30,7 +29,7 @@ class CacheCallbackHandler(
         active_cache = self._get_active_cache(cache=self._cache)
         return active_cache
 
-    def execute(self, resources: CallbackResourcesTContr):
+    def execute(self, resources: CacheCallbackResourcesTContr):
         self._clear()
         self._execute(resources=resources)
         self._update_cache()
@@ -41,7 +40,7 @@ class CacheCallbackHandler(
     def clear(self):
         self._clear()
 
-    def _execute(self, resources: CallbackResourcesTContr):
+    def _execute(self, resources: CacheCallbackResourcesTContr):
         super().execute(resources=resources)
 
     def _update_cache(self):
