588a258824de9b3d43c196653c00d24b790f9ab4
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-11 00:00:52 +0200

    Define tracking queue items for async logging

---
+++ artifact-experiment/artifact_experiment/base/tracking/background/item.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from typing import Dict, Generic, TypeVar
+
+from artifact_core.base.artifact_dependencies import ArtifactResult
+from matplotlib.figure import Figure
+from numpy import ndarray
+
+from artifact_experiment.base.entities.file import File
+from artifact_experiment.base.entities.tracking_data import TrackingData
+
+TrackingDataTCov = TypeVar("TrackingDataTCov", bound=TrackingData, covariant=True)
+
+
+@dataclass(frozen=True)
+class TrackingQueueItem(Generic[TrackingDataTCov]):
+    name: str
+    value: TrackingDataTCov
+
+
+ArtifactResultTCov = TypeVar("ArtifactResultTCov", bound=ArtifactResult, covariant=True)
+
+
+@dataclass(frozen=True)
+class ArtifactQueueItem(TrackingQueueItem[ArtifactResultTCov]):
+    pass
+
+
+@dataclass(frozen=True)
+class ScoreQueueItem(ArtifactQueueItem[float]):
+    pass
+
+
+@dataclass(frozen=True)
+class ArrayQueueItem(ArtifactQueueItem[ndarray]):
+    pass
+
+
+@dataclass(frozen=True)
+class PlotQueueItem(ArtifactQueueItem[Figure]):
+    pass
+
+
+@dataclass(frozen=True)
+class ScoreCollectionQueueItem(ArtifactQueueItem[Dict[str, float]]):
+    pass
+
+
+@dataclass(frozen=True)
+class ArrayCollectionQueueItem(ArtifactQueueItem[Dict[str, ndarray]]):
+    pass
+
+
+@dataclass(frozen=True)
+class PlotCollectionQueueItem(ArtifactQueueItem[Dict[str, Figure]]):
+    pass
+
+
+@dataclass(frozen=True)
+class FileQueueItem(TrackingQueueItem[File]):
+    pass
+
+
+@dataclass(frozen=True)
+class StopFlag(TrackingQueueItem[None]):
+    name: str = "stop_flag"
+    value: None = None
