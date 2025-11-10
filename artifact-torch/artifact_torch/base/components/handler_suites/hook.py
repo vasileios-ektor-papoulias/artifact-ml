3929f462baae93e2f1ae0925031176756be15dde
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-11 00:36:42 +0200

    Make handler suites adhere to new asbtraction

---
+++ artifact-torch/artifact_torch/base/components/handler_suites/hook.py
@@ -0,0 +1,59 @@
+from collections.abc import Sequence
+from typing import Any, Generic, Type, TypeVar
+
+from artifact_experiment.base.components.handler_suites.tracking import TrackingCallbackHandlerSuite
+
+from artifact_torch.base.components.callbacks.hook import (
+    HookArrayCallback,
+    HookArrayCollectionCallback,
+    HookCallback,
+    HookCallbackResources,
+    HookPlotCallback,
+    HookPlotCollectionCallback,
+    HookScoreCallback,
+    HookScoreCollectionCallback,
+)
+from artifact_torch.base.components.handlers.hook import (
+    HookCallbackHandler,
+)
+from artifact_torch.base.model.base import Model
+
+ModelTContr = TypeVar("ModelTContr", bound=Model, contravariant=True)
+HookCallbackHandlerSuiteT = TypeVar(
+    "HookCallbackHandlerSuiteT", bound="HookCallbackHandlerSuite[Any]"
+)
+
+
+class HookCallbackHandlerSuite(
+    TrackingCallbackHandlerSuite[
+        HookCallbackHandler[ModelTContr, Any],
+        HookCallback[ModelTContr, Any, Any],
+        HookCallbackResources[ModelTContr],
+    ],
+    Generic[ModelTContr],
+):
+    @classmethod
+    def build(
+        cls: Type[HookCallbackHandlerSuiteT],
+        score_callbacks: Sequence[HookScoreCallback[ModelTContr]],
+        array_callbacks: Sequence[HookArrayCallback[ModelTContr]],
+        plot_callbacks: Sequence[HookPlotCallback[ModelTContr]],
+        score_collection_callbacks: Sequence[HookScoreCollectionCallback[ModelTContr]],
+        array_collection_callbacks: Sequence[HookArrayCollectionCallback[ModelTContr]],
+        plot_collection_callbacks: Sequence[HookPlotCollectionCallback[ModelTContr]],
+    ) -> HookCallbackHandlerSuiteT:
+        handler_suite = cls(
+            score_handler=HookCallbackHandler(callbacks=score_callbacks),
+            array_handler=HookCallbackHandler(callbacks=array_callbacks),
+            plot_handler=HookCallbackHandler(callbacks=plot_callbacks),
+            score_collection_handler=HookCallbackHandler(callbacks=score_collection_callbacks),
+            array_collection_handler=HookCallbackHandler(callbacks=array_collection_callbacks),
+            plot_collection_handler=HookCallbackHandler(callbacks=plot_collection_callbacks),
+        )
+        return handler_suite
+
+    def attach(self, resources: HookCallbackResources[ModelTContr]) -> bool:
+        any_attached = False
+        for handler in self._ls_handlers:
+            any_attached |= handler.attach(resources=resources)
+        return any_attached
