174110644e435e6d00f531d74e2d0a22e1118980
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-11 00:37:40 +0200

    Adapt plans

--- artifact-torch/artifact_torch/base/components/plans/forward_hook.py
+++ artifact-torch/artifact_torch/base/components/plans/forward_hook.py
@@ -1,9 +1,9 @@
 from abc import abstractmethod
-from typing import Any, Generic, Optional, Sequence, Type, TypeVar
+from dataclasses import dataclass
+from typing import Any, Generic, Sequence, Type, TypeVar
 
-from artifact_experiment.base.entities.data_split import DataSplit
-from artifact_experiment.base.plans.base import CallbackExecutionPlan
-from artifact_experiment.base.tracking.client import TrackingClient
+from artifact_core.base.artifact_dependencies import ArtifactResult
+from artifact_experiment.base.components.plans.base import CallbackExecutionPlan, PlanBuildContext
 
 from artifact_torch.base.components.callbacks.forward_hook import (
     ForwardHookArrayCallback,
@@ -15,11 +15,14 @@ from artifact_torch.base.components.callbacks.forward_hook import (
     ForwardHookScoreCollectionCallback,
 )
 from artifact_torch.base.components.callbacks.hook import HookCallbackResources
-from artifact_torch.base.components.handlers.hook import (
-    HookCallbackHandler,
-    HookCallbackHandlerSuite,
-)
+from artifact_torch.base.components.handler_suites.hook import HookCallbackHandlerSuite
 from artifact_torch.base.model.base import Model
+
+
+@dataclass(frozen=True)
+class ForwardHookPlanBuildContext(PlanBuildContext):
+    pass
+
 
 ModelTContr = TypeVar("ModelTContr", bound=Model[Any, Any], contravariant=True)
 ForwardHookPlanT = TypeVar("ForwardHookPlanT", bound="ForwardHookPlan[Any]")
@@ -27,78 +30,52 @@ ForwardHookPlanT = TypeVar("ForwardHookPlanT", bound="ForwardHookPlan[Any]")
 
 class ForwardHookPlan(
     CallbackExecutionPlan[
-        HookCallbackHandler[ModelTContr, Any],
-        ForwardHookCallback[ModelTContr, Any, Any],
+        HookCallbackHandlerSuite[ModelTContr],
+        ForwardHookCallback[ModelTContr, ArtifactResult, Any],
         HookCallbackResources[ModelTContr],
+        ForwardHookPlanBuildContext,
     ],
     Generic[ModelTContr],
 ):
-    def __init__(
-        self,
-        handler_suite: HookCallbackHandlerSuite[ModelTContr],
-        data_split: Optional[DataSplit] = None,
-        tracking_client: Optional[TrackingClient] = None,
-    ):
-        super().__init__(
-            handler_suite=handler_suite,
-            data_split=data_split,
-            tracking_client=tracking_client,
-        )
+    @classmethod
+    @abstractmethod
+    def _get_score_callbacks(
+        cls, context: ForwardHookPlanBuildContext
+    ) -> Sequence[ForwardHookScoreCallback[ModelTContr]]: ...
 
     @classmethod
-    def build(
-        cls: Type[ForwardHookPlanT], tracking_client: Optional[TrackingClient] = None
-    ) -> ForwardHookPlanT:
-        score_callbacks = cls._get_score_callbacks()
-        array_callbacks = cls._get_array_callbacks()
-        plot_callbacks = cls._get_plot_callbacks()
-        score_collection_callbacks = cls._get_score_collection_callbacks()
-        array_collection_callbacks = cls._get_array_collection_callbacks()
-        plot_collection_callbacks = cls._get_plot_collection_callbacks()
-        handler_suite = HookCallbackHandlerSuite[ModelTContr].build(
-            score_callbacks=score_callbacks,
-            array_callbacks=array_callbacks,
-            plot_callbacks=plot_callbacks,
-            score_collection_callbacks=score_collection_callbacks,
-            array_collection_callbacks=array_collection_callbacks,
-            plot_collection_callbacks=plot_collection_callbacks,
-            tracking_client=tracking_client,
-        )
-        plan = cls(handler_suite=handler_suite, tracking_client=tracking_client)
-        return plan
-
-    @staticmethod
     @abstractmethod
-    def _get_score_callbacks() -> Sequence[ForwardHookScoreCallback[ModelTContr]]: ...
+    def _get_array_callbacks(
+        cls, context: ForwardHookPlanBuildContext
+    ) -> Sequence[ForwardHookArrayCallback[ModelTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_array_callbacks() -> Sequence[ForwardHookArrayCallback[ModelTContr]]: ...
+    def _get_plot_callbacks(
+        cls, context: ForwardHookPlanBuildContext
+    ) -> Sequence[ForwardHookPlotCallback[ModelTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_plot_callbacks() -> Sequence[ForwardHookPlotCallback[ModelTContr]]: ...
+    def _get_score_collection_callbacks(
+        cls, context: ForwardHookPlanBuildContext
+    ) -> Sequence[ForwardHookScoreCollectionCallback[ModelTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_score_collection_callbacks() -> Sequence[
-        ForwardHookScoreCollectionCallback[ModelTContr]
-    ]: ...
+    def _get_array_collection_callbacks(
+        cls, context: ForwardHookPlanBuildContext
+    ) -> Sequence[ForwardHookArrayCollectionCallback[ModelTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_array_collection_callbacks() -> Sequence[
-        ForwardHookArrayCollectionCallback[ModelTContr]
-    ]: ...
-
-    @staticmethod
-    @abstractmethod
-    def _get_plot_collection_callbacks() -> Sequence[
-        ForwardHookPlotCollectionCallback[ModelTContr]
-    ]: ...
+    def _get_plot_collection_callbacks(
+        cls, context: ForwardHookPlanBuildContext
+    ) -> Sequence[ForwardHookPlotCollectionCallback[ModelTContr]]: ...
 
     def attach(self, resources: HookCallbackResources[ModelTContr]) -> bool:
-        any_attached = False
-        for handler in self._ls_handlers:
-            any_attached |= handler.attach(resources=resources)
-        return any_attached
+        return self._handler_suite.attach(resources=resources)
+
+    @classmethod
+    def _get_handler_suite_type(cls) -> Type[HookCallbackHandlerSuite[ModelTContr]]:
+        return HookCallbackHandlerSuite
--- artifact-torch/artifact_torch/base/components/plans/model_io.py
+++ artifact-torch/artifact_torch/base/components/plans/model_io.py
@@ -1,8 +1,9 @@
 from abc import abstractmethod
-from typing import Any, Generic, Optional, Sequence, Type, TypeVar
+from dataclasses import dataclass
+from typing import Any, Generic, Sequence, Type, TypeVar
 
-from artifact_experiment.base.plans.base import CallbackExecutionPlan
-from artifact_experiment.base.tracking.client import TrackingClient
+from artifact_core.base.artifact_dependencies import ArtifactResult
+from artifact_experiment.base.components.plans.base import CallbackExecutionPlan, PlanBuildContext
 
 from artifact_torch.base.components.callbacks.model_io import (
     ModelIOArrayCallback,
@@ -14,11 +15,14 @@ from artifact_torch.base.components.callbacks.model_io import (
     ModelIOScoreCallback,
     ModelIOScoreCollectionCallback,
 )
-from artifact_torch.base.components.handlers.model_io import (
-    ModelIOCallbackHandler,
-    ModelIOCallbackHandlerSuite,
-)
+from artifact_torch.base.components.handler_suites.model_io import ModelIOCallbackHandlerSuite
 from artifact_torch.base.model.io import ModelInput, ModelOutput
+
+
+@dataclass(frozen=True)
+class ModelIOPlanBuildContext(PlanBuildContext):
+    pass
+
 
 ModelInputTContr = TypeVar("ModelInputTContr", bound=ModelInput, contravariant=True)
 ModelOutputTContr = TypeVar("ModelOutputTContr", bound=ModelOutput, contravariant=True)
@@ -27,79 +31,54 @@ ModelIOPlanT = TypeVar("ModelIOPlanT", bound="ModelIOPlan[Any, Any]")
 
 class ModelIOPlan(
     CallbackExecutionPlan[
-        ModelIOCallbackHandler[ModelOutputTContr, Any],
-        ModelIOCallback[ModelInputTContr, ModelOutputTContr, Any, Any],
+        ModelIOCallbackHandlerSuite[ModelInputTContr, ModelOutputTContr],
+        ModelIOCallback[ModelInputTContr, ModelOutputTContr, ArtifactResult, Any],
         ModelIOCallbackResources[ModelOutputTContr],
+        ModelIOPlanBuildContext,
     ],
     Generic[ModelInputTContr, ModelOutputTContr],
 ):
-    def __init__(
-        self,
-        handler_suite: ModelIOCallbackHandlerSuite[ModelInputTContr, ModelOutputTContr],
-        tracking_client: Optional[TrackingClient] = None,
-    ):
-        super().__init__(handler_suite=handler_suite, tracking_client=tracking_client)
+    @classmethod
+    @abstractmethod
+    def _get_score_callbacks(
+        cls, context: ModelIOPlanBuildContext
+    ) -> Sequence[ModelIOScoreCallback[ModelInputTContr, ModelOutputTContr]]: ...
 
     @classmethod
-    def build(
-        cls: Type[ModelIOPlanT], tracking_client: Optional[TrackingClient] = None
-    ) -> ModelIOPlanT:
-        score_callbacks = cls._get_score_callbacks()
-        array_callbacks = cls._get_array_callbacks()
-        plot_callbacks = cls._get_plot_callbacks()
-        score_collection_callbacks = cls._get_score_collection_callbacks()
-        array_collection_callbacks = cls._get_array_collection_callbacks()
-        plot_collection_callbacks = cls._get_plot_collection_callbacks()
-        handler_suite = ModelIOCallbackHandlerSuite[ModelInputTContr, ModelOutputTContr].build(
-            score_callbacks=score_callbacks,
-            array_callbacks=array_callbacks,
-            plot_callbacks=plot_callbacks,
-            score_collection_callbacks=score_collection_callbacks,
-            array_collection_callbacks=array_collection_callbacks,
-            plot_collection_callbacks=plot_collection_callbacks,
-            tracking_client=tracking_client,
-        )
-        plan = cls(handler_suite=handler_suite, tracking_client=tracking_client)
-        return plan
-
-    @staticmethod
     @abstractmethod
-    def _get_score_callbacks() -> Sequence[
-        ModelIOScoreCallback[ModelInputTContr, ModelOutputTContr]
-    ]: ...
+    def _get_array_callbacks(
+        cls, context: ModelIOPlanBuildContext
+    ) -> Sequence[ModelIOArrayCallback[ModelInputTContr, ModelOutputTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_array_callbacks() -> Sequence[
-        ModelIOArrayCallback[ModelInputTContr, ModelOutputTContr]
-    ]: ...
+    def _get_plot_callbacks(
+        cls, context: ModelIOPlanBuildContext
+    ) -> Sequence[ModelIOPlotCallback[ModelInputTContr, ModelOutputTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_plot_callbacks() -> Sequence[
-        ModelIOPlotCallback[ModelInputTContr, ModelOutputTContr]
-    ]: ...
+    def _get_score_collection_callbacks(
+        cls, context: ModelIOPlanBuildContext
+    ) -> Sequence[ModelIOScoreCollectionCallback[ModelInputTContr, ModelOutputTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_score_collection_callbacks() -> Sequence[
-        ModelIOScoreCollectionCallback[ModelInputTContr, ModelOutputTContr]
-    ]: ...
+    def _get_array_collection_callbacks(
+        cls, context: ModelIOPlanBuildContext
+    ) -> Sequence[ModelIOArrayCollectionCallback[ModelInputTContr, ModelOutputTContr]]: ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def _get_array_collection_callbacks() -> Sequence[
-        ModelIOArrayCollectionCallback[ModelInputTContr, ModelOutputTContr]
-    ]: ...
-
-    @staticmethod
-    @abstractmethod
-    def _get_plot_collection_callbacks() -> Sequence[
-        ModelIOPlotCollectionCallback[ModelInputTContr, ModelOutputTContr]
-    ]: ...
+    def _get_plot_collection_callbacks(
+        cls, context: ModelIOPlanBuildContext
+    ) -> Sequence[ModelIOPlotCollectionCallback[ModelInputTContr, ModelOutputTContr]]: ...
 
     def attach(self, resources: ModelIOCallbackResources[ModelOutputTContr]) -> bool:
-        any_attached = False
-        for handler in self._ls_handlers:
-            any_attached |= handler.attach(resources=resources)
-        return any_attached
+        return self._handler_suite.attach(resources=resources)
+
+    @classmethod
+    def _get_handler_suite_type(
+        cls,
+    ) -> Type[ModelIOCallbackHandlerSuite[ModelInputTContr, ModelOutputTContr]]:
+        return ModelIOCallbackHandlerSuite
