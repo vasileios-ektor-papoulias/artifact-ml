a97fa664dd1dba17367ad73ba38e38a3e8cf6ca1
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-29 23:16:35 +0200

    Clean up registry types by introducing the repository concept

--- artifact-core/artifact_core/_base/orchestration/repository_reader.py
+++ artifact-core/artifact_core/_base/orchestration/repository_reader.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Generic, Mapping, Optional, Type, TypeVar, Union
+from typing import Any, Generic, Mapping, Optional, Type, TypeVar, Union
 
 from artifact_core._base.core.artifact import Artifact
 from artifact_core._base.core.hyperparams import (
@@ -9,28 +9,20 @@ from artifact_core._base.core.hyperparams import (
 from artifact_core._base.core.resource_spec import ResourceSpecProtocol
 from artifact_core._base.core.resources import ArtifactResources
 from artifact_core._base.orchestration.key_formatter import ArtifactKeyFormatter
-from artifact_core._base.primitives.artifact_type import ArtifactType
-from artifact_core._base.typing.artifact_result import (
-    ArtifactResult,
+from artifact_core._base.orchestration.repository import (
+    ArtifactHyperparamsRepository,
+    ArtifactRepository,
 )
+from artifact_core._base.primitives.artifact_type import ArtifactType
+from artifact_core._base.typing.artifact_result import ArtifactResult
 
 ArtifactResourcesT = TypeVar("ArtifactResourcesT", bound=ArtifactResources)
 ResourceSpecProtocolT = TypeVar("ResourceSpecProtocolT", bound=ResourceSpecProtocol)
 ArtifactTypeT = TypeVar("ArtifactTypeT", bound=ArtifactType)
 ArtifactResultT = TypeVar("ArtifactResultT", bound=ArtifactResult)
 
-ArtifactCollection = Dict[
-    str,
-    Type[Artifact[ArtifactResourcesT, ResourceSpecProtocolT, ArtifactHyperparams, ArtifactResultT]],
-]
-ArtifactHyperparamsCollection = Dict[str, Type[ArtifactHyperparams]]
-Registree = Union[Artifact[Any, Any, Any, Any], ArtifactHyperparams]
-RegistreeT = TypeVar("RegistreeT", bound=Registree)
-ArtifactT = TypeVar("ArtifactT", bound=Artifact[Any, Any, Any, Any])
-ArtifactHyperparamsT = TypeVar("ArtifactHyperparamsT", bound=ArtifactHyperparams)
 
-
-class ArtifactRegistryReader(
+class ArtifactRepositoryReader(
     Generic[ArtifactResourcesT, ResourceSpecProtocolT, ArtifactTypeT, ArtifactResultT]
 ):
     @classmethod
@@ -38,16 +30,16 @@ class ArtifactRegistryReader(
         cls,
         artifact_type: Union[ArtifactTypeT, str],
         resource_spec: ResourceSpecProtocolT,
-        artifact_registry: ArtifactCollection,
-        artifact_hyperparams_registry: ArtifactHyperparamsCollection,
+        artifact_repository: ArtifactRepository,
+        artifact_hyperparams_repository: ArtifactHyperparamsRepository,
         artifact_configurations: Mapping[str, Mapping[str, Any]],
     ) -> Artifact[ArtifactResourcesT, ResourceSpecProtocolT, ArtifactHyperparams, ArtifactResultT]:
         key = ArtifactKeyFormatter.get_artifact_key(artifact_type=artifact_type)
         return cls._get(
             key=key,
             resource_spec=resource_spec,
-            artifact_registry=artifact_registry,
-            artifact_hyperparams_registry=artifact_hyperparams_registry,
+            artifact_repository=artifact_repository,
+            artifact_hyperparams_repository=artifact_hyperparams_repository,
             artifact_configurations=artifact_configurations,
         )
 
@@ -56,16 +48,16 @@ class ArtifactRegistryReader(
         cls,
         key: str,
         resource_spec: ResourceSpecProtocolT,
-        artifact_registry: ArtifactCollection,
-        artifact_hyperparams_registry: ArtifactHyperparamsCollection,
+        artifact_repository: ArtifactRepository,
+        artifact_hyperparams_repository: ArtifactHyperparamsRepository,
         artifact_configurations: Mapping[str, Mapping[str, Any]],
     ) -> Artifact[ArtifactResourcesT, ResourceSpecProtocolT, ArtifactHyperparams, ArtifactResultT]:
-        artifact_class = cls._get_artifact_class(key=key, registry=artifact_registry)
+        artifact_class = cls._get_artifact_class(key=key, repository=artifact_repository)
         artifact_config = cls._get_artifact_config(
             key=key, artifact_configurations=artifact_configurations
         )
         artifact_hyperparams_class = cls._get_artifact_hyperparams_class(
-            key=key, registry=artifact_hyperparams_registry
+            key=key, repository=artifact_hyperparams_repository
         )
         hyperparams = cls._build_artifact_hyperparams(
             artifact_hyperparams_class=artifact_hyperparams_class, artifact_config=artifact_config
@@ -75,20 +67,20 @@ class ArtifactRegistryReader(
 
     @classmethod
     def _get_artifact_class(
-        cls, key: str, registry: ArtifactCollection
+        cls, key: str, repository: ArtifactRepository
     ) -> Type[
         Artifact[ArtifactResourcesT, ResourceSpecProtocolT, ArtifactHyperparams, ArtifactResultT]
     ]:
-        artifact_class = registry.get(key, None)
+        artifact_class = repository.get(key, None)
         if artifact_class is None:
             raise ValueError(f"Artifact {key} not registered")
         return artifact_class
 
     @classmethod
     def _get_artifact_hyperparams_class(
-        cls, key: str, registry: ArtifactHyperparamsCollection
+        cls, key: str, repository: ArtifactHyperparamsRepository
     ) -> Type[ArtifactHyperparams]:
-        artifact_hyperparams_class = registry.get(key, NoArtifactHyperparams)
+        artifact_hyperparams_class = repository.get(key, NoArtifactHyperparams)
         return artifact_hyperparams_class
 
     @classmethod
