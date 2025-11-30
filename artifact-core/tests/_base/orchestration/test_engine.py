b98d772b4979c9db80ce6a8f9243bca57a870013
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-11-30 22:30:29 +0200

    Refactor artifact engine tests: do not touch internals

--- artifact-core/tests/_base/orchestration/test_engine.py
+++ artifact-core/tests/_base/orchestration/test_engine.py
@@ -1,4 +1,4 @@
-from typing import Callable, Tuple
+from typing import Any, Callable, Tuple, Union
 from unittest.mock import MagicMock
 
 import numpy as np
@@ -14,49 +14,85 @@ from artifact_core._base.typing.artifact_result import (
 from matplotlib.figure import Figure
 from pytest_mock import MockerFixture
 
+from tests._base.dummy.artifacts.base import DummyArtifact
 from tests._base.dummy.engine.engine import DummyArtifactEngine
+from tests._base.dummy.registries.base import DummyArtifactRegistry
 from tests._base.dummy.resource_spec import DummyResourceSpec
 from tests._base.dummy.resources import DummyArtifactResources
+from tests._base.dummy.types.array_collections import DummyArrayCollectionType
+from tests._base.dummy.types.arrays import DummyArrayType
+from tests._base.dummy.types.plot_collections import DummyPlotCollectionType
+from tests._base.dummy.types.plots import DummyPlotType
+from tests._base.dummy.types.score_collections import DummyScoreCollectionType
+from tests._base.dummy.types.scores import DummyScoreType
 
 
 @pytest.fixture
-def mock_dependency_factory(
+def engine_factory(
     mocker: MockerFixture,
-) -> Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]]:
-    def _factory(return_value: ArtifactResult) -> Tuple[MagicMock, MagicMock, MagicMock]:
-        artifact_type = mocker.Mock(name="dummy_artifact_type")
-        artifact = mocker.Mock(name="dummy_artifact")
+) -> Callable[
+    [str, DummyResourceSpec, ArtifactResult],
+    Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+]:
+    def _factory(
+        modality: str,
+        resource_spec: DummyResourceSpec,
+        return_value: ArtifactResult,
+    ) -> Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine]:
+        artifact = mocker.Mock(name="artifact", spec_set=DummyArtifact[Any, Any])
+        registry = mocker.Mock(name="registry", spec_set=DummyArtifactRegistry[Any, Any])
+        other_registry = mocker.Mock(
+            name="other_registry", spec_set=DummyArtifactRegistry[Any, Any]
+        )
         artifact.compute.return_value = return_value
-
-        registry = mocker.Mock(name="dummy_registry")
         registry.get.return_value = artifact
-
-        return artifact_type, artifact, registry
+        registries = {
+            f"{modality}_registry": other_registry
+            for modality in [
+                "score",
+                "array",
+                "plot",
+                "score_collection",
+                "array_collection",
+                "plot_collection",
+            ]
+        }
+        registries[f"{modality}_registry"] = registry
+        engine = DummyArtifactEngine(resource_spec=resource_spec, **registries)
+        return artifact, registry, other_registry, engine
 
     return _factory
 
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "resource_spec, resources, expected_result",
+    "artifact_type, resource_spec, resources, expected_result",
     [
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=1), 1),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=10), 2),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=False, x=1), 3),
+        ("SCORE_TYPE_1", DummyResourceSpec(scale=1.0), DummyArtifactResources(valid=True, x=1), 1),
+        ("SCORE_TYPE_2", DummyResourceSpec(scale=2.0), DummyArtifactResources(valid=True, x=2), 2),
+        (
+            "CUSTOM_SCORE_ARTIFACT",
+            DummyResourceSpec(scale=3.0),
+            DummyArtifactResources(valid=False, x=3),
+            3,
+        ),
     ],
 )
 def test_produce_score(
-    mock_dependency_factory: Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]],
+    engine_factory: Callable[
+        [str, DummyResourceSpec, ArtifactResult],
+        Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+    ],
+    artifact_type: Union[DummyScoreType, str],
     resource_spec: DummyResourceSpec,
     resources: DummyArtifactResources,
     expected_result: float,
 ):
-    artifact_type, artifact, registry = mock_dependency_factory(expected_result)
-
-    engine = DummyArtifactEngine(resource_spec=resource_spec)
-    engine._score_registry = registry  # type: ignore
+    artifact, registry, other_registry, engine = engine_factory(
+        "score", resource_spec, expected_result
+    )
     result = engine.produce_score(score_type=artifact_type, resources=resources)
-
+    other_registry.get.assert_not_called()
     registry.get.assert_called_once_with(artifact_type=artifact_type, resource_spec=resource_spec)
     artifact.compute.assert_called_once_with(resources=resources)
     assert result == expected_result
@@ -64,25 +100,43 @@ def test_produce_score(
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "resource_spec, resources, expected_result",
+    "artifact_type, resource_spec, resources, expected_result",
     [
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=1), np.array([1])),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=10), np.array([10, 20])),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=False, x=1), np.array([1])),
+        (
+            "ARRAY_TYPE_1",
+            DummyResourceSpec(scale=1.0),
+            DummyArtifactResources(valid=True, x=1),
+            np.array([1]),
+        ),
+        (
+            "ARRAY_TYPE_2",
+            DummyResourceSpec(scale=2.0),
+            DummyArtifactResources(valid=True, x=10),
+            np.array([10, 20]),
+        ),
+        (
+            "CUSTOM_ARRAY",
+            DummyResourceSpec(scale=3.0),
+            DummyArtifactResources(valid=False, x=3),
+            np.array([1, 2, 3]),
+        ),
     ],
 )
 def test_produce_array(
-    mock_dependency_factory: Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]],
+    engine_factory: Callable[
+        [str, DummyResourceSpec, ArtifactResult],
+        Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+    ],
+    artifact_type: Union[DummyArrayType, str],
     resource_spec: DummyResourceSpec,
     resources: DummyArtifactResources,
     expected_result: Array,
 ):
-    artifact_type, artifact, registry = mock_dependency_factory(expected_result)
-
-    engine = DummyArtifactEngine(resource_spec=resource_spec)
-    engine._array_registry = registry  # type: ignore
+    artifact, registry, other_registry, engine = engine_factory(
+        "array", resource_spec, expected_result
+    )
     result = engine.produce_array(array_type=artifact_type, resources=resources)
-
+    other_registry.get.assert_not_called()
     registry.get.assert_called_once_with(artifact_type=artifact_type, resource_spec=resource_spec)
     artifact.compute.assert_called_once_with(resources=resources)
     assert (result == expected_result).all()
@@ -90,25 +144,43 @@ def test_produce_array(
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "resource_spec, resources, expected_result",
+    "artifact_type, resource_spec, resources, expected_result",
     [
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=1), Figure()),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=10), Figure()),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=False, x=1), Figure()),
+        (
+            "PLOT_TYPE_1",
+            DummyResourceSpec(scale=1.0),
+            DummyArtifactResources(valid=True, x=1),
+            Figure(),
+        ),
+        (
+            "PLOT_TYPE_2",
+            DummyResourceSpec(scale=2.0),
+            DummyArtifactResources(valid=True, x=10),
+            Figure(),
+        ),
+        (
+            "CUSTOM_PLOT",
+            DummyResourceSpec(scale=3.0),
+            DummyArtifactResources(valid=False, x=3),
+            Figure(),
+        ),
     ],
 )
 def test_produce_plot(
-    mock_dependency_factory: Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]],
+    engine_factory: Callable[
+        [str, DummyResourceSpec, ArtifactResult],
+        Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+    ],
+    artifact_type: Union[DummyPlotType, str],
     resource_spec: DummyResourceSpec,
     resources: DummyArtifactResources,
     expected_result: Plot,
 ):
-    artifact_type, artifact, registry = mock_dependency_factory(expected_result)
-
-    engine = DummyArtifactEngine(resource_spec=resource_spec)
-    engine._plot_registry = registry  # type: ignore
+    artifact, registry, other_registry, engine = engine_factory(
+        "plot", resource_spec, expected_result
+    )
     result = engine.produce_plot(plot_type=artifact_type, resources=resources)
-
+    other_registry.get.assert_not_called()
     registry.get.assert_called_once_with(artifact_type=artifact_type, resource_spec=resource_spec)
     artifact.compute.assert_called_once_with(resources=resources)
     assert result == expected_result
@@ -116,27 +188,45 @@ def test_produce_plot(
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "resource_spec, resources, expected_result",
+    "artifact_type, resource_spec, resources, expected_result",
     [
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=1), {"item": 1}),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=10), {"item": 2}),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=False, x=1), {"item": 3}),
+        (
+            "SCORE_COLLECTION_TYPE_1",
+            DummyResourceSpec(scale=1.0),
+            DummyArtifactResources(valid=True, x=1),
+            {"item": 1},
+        ),
+        (
+            "SCORE_COLLECTION_TYPE_2",
+            DummyResourceSpec(scale=2.0),
+            DummyArtifactResources(valid=True, x=10),
+            {"item": 2},
+        ),
+        (
+            "CUSTOM_SCORE_COLLECTION",
+            DummyResourceSpec(scale=3.0),
+            DummyArtifactResources(valid=False, x=3),
+            {"item": 3},
+        ),
     ],
 )
 def test_produce_score_collection(
-    mock_dependency_factory: Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]],
+    engine_factory: Callable[
+        [str, DummyResourceSpec, ArtifactResult],
+        Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+    ],
+    artifact_type: Union[DummyScoreCollectionType, str],
     resource_spec: DummyResourceSpec,
     resources: DummyArtifactResources,
     expected_result: ScoreCollection,
 ):
-    artifact_type, artifact, registry = mock_dependency_factory(expected_result)
-
-    engine = DummyArtifactEngine(resource_spec=resource_spec)
-    engine._score_collection_registry = registry  # type: ignore
+    artifact, registry, other_registry, engine = engine_factory(
+        "score_collection", resource_spec, expected_result
+    )
     result = engine.produce_score_collection(
         score_collection_type=artifact_type, resources=resources
     )
-
+    other_registry.get.assert_not_called()
     registry.get.assert_called_once_with(artifact_type=artifact_type, resource_spec=resource_spec)
     artifact.compute.assert_called_once_with(resources=resources)
     assert result == expected_result
@@ -144,39 +234,45 @@ def test_produce_score_collection(
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "resource_spec, resources, expected_result",
+    "artifact_type, resource_spec, resources, expected_result",
     [
         (
-            DummyResourceSpec(scale=1),
+            "ARRAY_COLLECTION_TYPE_1",
+            DummyResourceSpec(scale=1.0),
             DummyArtifactResources(valid=True, x=1),
             {"item": np.array([1])},
         ),
         (
-            DummyResourceSpec(scale=1),
+            "ARRAY_COLLECTION_TYPE_2",
+            DummyResourceSpec(scale=2.0),
             DummyArtifactResources(valid=True, x=10),
             {"item": np.array([10])},
         ),
         (
-            DummyResourceSpec(scale=1),
-            DummyArtifactResources(valid=False, x=1),
-            {"item": np.array([1])},
+            "CUSTOM_ARRAY_COLLECTION",
+            DummyResourceSpec(scale=3.0),
+            DummyArtifactResources(valid=False, x=3),
+            {"item": np.array([1, 2, 3])},
         ),
     ],
 )
 def test_produce_array_collection(
-    mock_dependency_factory: Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]],
+    engine_factory: Callable[
+        [str, DummyResourceSpec, ArtifactResult],
+        Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+    ],
+    artifact_type: Union[DummyArrayCollectionType, str],
     resource_spec: DummyResourceSpec,
     resources: DummyArtifactResources,
     expected_result: ArrayCollection,
 ):
-    artifact_type, artifact, registry = mock_dependency_factory(expected_result)
-
-    engine = DummyArtifactEngine(resource_spec=resource_spec)
-    engine._array_collection_registry = registry  # type: ignore
+    artifact, registry, other_registry, engine = engine_factory(
+        "array_collection", resource_spec, expected_result
+    )
     result = engine.produce_array_collection(
         array_collection_type=artifact_type, resources=resources
     )
-
+    other_registry.get.assert_not_called()
     registry.get.assert_called_once_with(artifact_type=artifact_type, resource_spec=resource_spec)
     artifact.compute.assert_called_once_with(resources=resources)
     for array_name in result.keys():
@@ -185,25 +281,43 @@ def test_produce_array_collection(
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    "resource_spec, resources, expected_result",
+    "artifact_type, resource_spec, resources, expected_result",
     [
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=1), {"item": Figure()}),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=True, x=10), {"item": Figure()}),
-        (DummyResourceSpec(scale=1), DummyArtifactResources(valid=False, x=1), {"item": Figure()}),
+        (
+            "PLOT_COLLECTION_TYPE_1",
+            DummyResourceSpec(scale=1.0),
+            DummyArtifactResources(valid=True, x=1),
+            {"item": Figure()},
+        ),
+        (
+            "PLOT_COLLECTION_TYPE_2",
+            DummyResourceSpec(scale=2.0),
+            DummyArtifactResources(valid=True, x=10),
+            {"item": Figure()},
+        ),
+        (
+            "CUSTOM_PLOT_COLLECTION",
+            DummyResourceSpec(scale=3.0),
+            DummyArtifactResources(valid=False, x=3),
+            {"item": Figure()},
+        ),
     ],
 )
 def test_produce_plot_collection(
-    mock_dependency_factory: Callable[[ArtifactResult], Tuple[MagicMock, MagicMock, MagicMock]],
+    engine_factory: Callable[
+        [str, DummyResourceSpec, ArtifactResult],
+        Tuple[MagicMock, MagicMock, MagicMock, DummyArtifactEngine],
+    ],
+    artifact_type: Union[DummyPlotCollectionType, str],
     resource_spec: DummyResourceSpec,
     resources: DummyArtifactResources,
     expected_result: PlotCollection,
 ):
-    artifact_type, artifact, registry = mock_dependency_factory(expected_result)
-
-    engine = DummyArtifactEngine(resource_spec=resource_spec)
-    engine._plot_collection_registry = registry  # type: ignore
+    artifact, registry, other_registry, engine = engine_factory(
+        "plot_collection", resource_spec, expected_result
+    )
     result = engine.produce_plot_collection(plot_collection_type=artifact_type, resources=resources)
-
+    other_registry.get.assert_not_called()
     registry.get.assert_called_once_with(artifact_type=artifact_type, resource_spec=resource_spec)
     artifact.compute.assert_called_once_with(resources=resources)
     assert result == expected_result
