c4cfef2d6b85106ed773ef8d6d8484bf8b652da1
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-12-01 15:07:21 +0200

    Adapt pattern to leverage pytest functionality

--- artifact-core/tests/_bootstrap/config/test_override_locator.py
+++ artifact-core/tests/_bootstrap/config/test_override_locator.py
@@ -1,25 +1,33 @@
 from pathlib import Path
-from typing import Optional
-from unittest.mock import patch
+from typing import Callable, Optional
+from unittest.mock import MagicMock
 
 import pytest
 from artifact_core._bootstrap.config.override_locator import ConfigOverrideLocator
+from pytest_mock import MockerFixture
+
+
+@pytest.fixture
+def mock_directory_locator(
+    mocker: MockerFixture,
+) -> Callable[[Optional[Path]], MagicMock]:
+    def _mock_find(return_value: Optional[Path]) -> MagicMock:
+        patch_path = "artifact_core._utils.filesystem.directory_locator.DirectoryLocator.find"
+        return mocker.patch(patch_path, return_value=return_value)
+
+    return _mock_find
 
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
     "mock_return_value",
-    [
-        Path("/home/user/project/.artifact-ml"),
-        Path("/tmp/.artifact-ml"),
-        None,
-    ],
+    [Path("/home/user/project/.artifact-ml"), Path("/tmp/.artifact-ml"), None],
 )
-def test_find(mock_return_value: Optional[Path]):
-    with patch(
-        "artifact_core._bootstrap.config.override_locator.DirectoryLocator.find"
-    ) as mock_find:
-        mock_find.return_value = mock_return_value
-        result = ConfigOverrideLocator.find()
-        mock_find.assert_called_once_with(marker=".artifact-ml")
-        assert result == mock_return_value
+def test_find(
+    mock_directory_locator: Callable[[Optional[Path]], MagicMock],
+    mock_return_value: Optional[Path],
+):
+    mock_find = mock_directory_locator(mock_return_value)
+    result = ConfigOverrideLocator.find()
+    mock_find.assert_called_once_with(marker=".artifact-ml")
+    assert result == mock_return_value
