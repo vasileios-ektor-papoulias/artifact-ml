f37981f662148df6d747ebcf631c612601609cbd
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-12-06 16:15:48 +0200

    Adapt binary classification interfaces

--- artifact-core/artifact_core/binary_classification/_registries/base.py
+++ artifact-core/artifact_core/binary_classification/_registries/base.py
@@ -14,14 +14,21 @@ from artifact_core._domains.classification.registry import ClassificationArtifac
 from artifact_core._libs.resource_specs.binary_classification.protocol import (
     BinaryClassSpecProtocol,
 )
-from artifact_core.binary_classification._resources import BinaryClassificationArtifactResources
+from artifact_core._libs.resources.binary_classification.class_store import BinaryClassStore
+from artifact_core._libs.resources.binary_classification.classification_results import (
+    BinaryClassificationResults,
+)
 
 ArtifactTypeT = TypeVar("ArtifactTypeT", bound=ArtifactType)
 ArtifactResultT = TypeVar("ArtifactResultT", bound=ArtifactResult)
 
 
 BinaryClassificationArtifactRegistry = ClassificationArtifactRegistry[
-    BinaryClassificationArtifactResources, BinaryClassSpecProtocol, ArtifactTypeT, ArtifactResultT
+    BinaryClassStore,
+    BinaryClassificationResults,
+    BinaryClassSpecProtocol,
+    ArtifactTypeT,
+    ArtifactResultT,
 ]
 
 BinaryClassificationScoreRegistryBase = BinaryClassificationArtifactRegistry[ArtifactTypeT, Score]
