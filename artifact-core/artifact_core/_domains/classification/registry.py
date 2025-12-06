f30f3e8293ecf0f396084dde46536c8a84725658
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-12-06 16:15:23 +0200

    Align core classification interfaces with the pattern useed in the dataset comparison toolkit

--- artifact-core/artifact_core/_domains/classification/registry.py
+++ artifact-core/artifact_core/_domains/classification/registry.py
@@ -5,15 +5,24 @@ from artifact_core._base.primitives.artifact_type import ArtifactType
 from artifact_core._base.typing.artifact_result import ArtifactResult
 from artifact_core._domains.classification.artifact import ClassificationArtifactResources
 from artifact_core._libs.resource_specs.classification.protocol import ClassSpecProtocol
-
-ClassificationArtifactResourcesT = TypeVar(
-    "ClassificationArtifactResourcesT", bound=ClassificationArtifactResources
+from artifact_core._libs.resources.classification.class_store import ClassStore
+from artifact_core._libs.resources.classification.classification_results import (
+    ClassificationResults,
 )
+
+ClassStoreT = TypeVar("ClassStoreT", bound=ClassStore)
+ClassificationResultsT = TypeVar("ClassificationResultsT", bound=ClassificationResults)
 ClassSpecProtocolT = TypeVar("ClassSpecProtocolT", bound=ClassSpecProtocol)
 ArtifactTypeT = TypeVar("ArtifactTypeT", bound=ArtifactType)
 ArtifactResultT = TypeVar("ArtifactResultT", bound=ArtifactResult)
 
 
 ClassificationArtifactRegistry = ArtifactRegistry[
-    ClassificationArtifactResourcesT, ClassSpecProtocolT, ArtifactTypeT, ArtifactResultT
+    ClassificationArtifactResources[
+        ClassStoreT,
+        ClassificationResultsT,
+    ],
+    ClassSpecProtocolT,
+    ArtifactTypeT,
+    ArtifactResultT,
 ]
