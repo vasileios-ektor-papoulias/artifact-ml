7ec547f664bc4276908d8bd0c3dac2464aaa8d7b
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-12-08 00:17:58 +0200

    Add new test cases

--- artifact-core/tests/_libs/artifacts/binary_classification/conftest.py
+++ artifact-core/tests/_libs/artifacts/binary_classification/conftest.py
@@ -46,6 +46,38 @@ def binary_data_perfect() -> BinaryDataTuple:
 
 
 @pytest.fixture
+def binary_data_perfect_small() -> BinaryDataTuple:
+    id_to_is_pos: Dict[Hashable, bool] = {0: True, 1: False}
+    id_to_pred_pos: Dict[Hashable, bool] = {0: True, 1: False}
+    id_to_prob_pos: Dict[Hashable, float] = {0: 0.9, 1: 0.1}
+    return id_to_is_pos, id_to_pred_pos, id_to_prob_pos
+
+
+@pytest.fixture
+def binary_data_mixed_equal() -> BinaryDataTuple:
+    id_to_is_pos: Dict[Hashable, bool] = {0: True, 1: True, 2: False, 3: False}
+    id_to_pred_pos: Dict[Hashable, bool] = {0: True, 1: False, 2: True, 3: False}
+    id_to_prob_pos: Dict[Hashable, float] = {0: 0.9, 1: 0.3, 2: 0.7, 3: 0.1}
+    return id_to_is_pos, id_to_pred_pos, id_to_prob_pos
+
+
+@pytest.fixture
+def binary_data_all_fn() -> BinaryDataTuple:
+    id_to_is_pos: Dict[Hashable, bool] = {0: True, 1: True}
+    id_to_pred_pos: Dict[Hashable, bool] = {0: False, 1: False}
+    id_to_prob_pos: Dict[Hashable, float] = {0: 0.3, 1: 0.2}
+    return id_to_is_pos, id_to_pred_pos, id_to_prob_pos
+
+
+@pytest.fixture
+def binary_data_all_fp() -> BinaryDataTuple:
+    id_to_is_pos: Dict[Hashable, bool] = {0: False, 1: False}
+    id_to_pred_pos: Dict[Hashable, bool] = {0: True, 1: True}
+    id_to_prob_pos: Dict[Hashable, float] = {0: 0.8, 1: 0.7}
+    return id_to_is_pos, id_to_pred_pos, id_to_prob_pos
+
+
+@pytest.fixture
 def binary_data_dispatcher(request: pytest.FixtureRequest) -> BinaryDataTuple:
     valid_fixtures = [
         "binary_data_balanced",
@@ -53,6 +85,10 @@ def binary_data_dispatcher(request: pytest.FixtureRequest) -> BinaryDataTuple:
         "binary_data_all_positive",
         "binary_data_all_negative",
         "binary_data_perfect",
+        "binary_data_perfect_small",
+        "binary_data_mixed_equal",
+        "binary_data_all_fn",
+        "binary_data_all_fp",
     ]
     if request.param not in valid_fixtures:
         raise ValueError(f"Fixture {request.param} not found. Valid: {valid_fixtures}")
