932782accd87bc78163af3d0b749c6d9a536e0af
vasileios-ektor-papoulias <hector.papoulias@gmail.com>
2025-12-07 23:52:44 +0200

    Rewrite test cases using nnary data tuple dispatcher fixture

--- artifact-core/tests/_libs/artifacts/binary_classification/confusion/test_plotter.py
+++ artifact-core/tests/_libs/artifacts/binary_classification/confusion/test_plotter.py
@@ -1,4 +1,4 @@
-from typing import Dict, Hashable, List, Tuple
+from typing import List
 
 import pytest
 from artifact_core._libs.artifacts.binary_classification.confusion.calculator import (
@@ -15,6 +15,8 @@ from artifact_core._libs.artifacts.binary_classification.confusion.raw import Ra
 from matplotlib.figure import Figure
 from pytest_mock import MockerFixture
 
+from tests._libs.artifacts.binary_classification.conftest import BinaryDataTuple
+
 
 @pytest.mark.unit
 @pytest.mark.parametrize("normalization", list(ConfusionMatrixNormalizationStrategy))
@@ -27,9 +29,7 @@ def test_plot(
     mocker: MockerFixture,
     set_agg_backend,
     close_all_figs_after_test,
-    binary_data_dispatcher: Tuple[
-        Dict[Hashable, bool], Dict[Hashable, bool], Dict[Hashable, float]
-    ],
+    binary_data_dispatcher: BinaryDataTuple,
     normalization: ConfusionMatrixNormalizationStrategy,
 ):
     id_to_is_pos, id_to_pred_pos, _ = binary_data_dispatcher
@@ -60,20 +60,23 @@ def test_plot(
     indirect=True,
 )
 def test_plot_multiple(
+    mocker: MockerFixture,
     set_agg_backend,
     close_all_figs_after_test,
-    binary_data_dispatcher: Tuple[
-        Dict[Hashable, bool], Dict[Hashable, bool], Dict[Hashable, float]
-    ],
+    binary_data_dispatcher: BinaryDataTuple,
     normalization_types: List[ConfusionMatrixNormalizationStrategy],
 ):
     id_to_is_pos, id_to_pred_pos, _ = binary_data_dispatcher
+    spy_normalized = mocker.spy(
+        obj=NormalizedConfusionCalculator, name="compute_normalized_confusion_matrix"
+    )
     result = ConfusionMatrixPlotter.plot_multiple(
         true=id_to_is_pos, predicted=id_to_pred_pos, normalization_types=normalization_types
     )
     assert set(result.keys()) == set(normalization_types)
     for _, fig in result.items():
         assert isinstance(fig, Figure)
+    assert spy_normalized.call_count == len(normalization_types)
 
 
 @pytest.mark.unit
@@ -83,11 +86,7 @@ def test_plot_multiple(
     indirect=True,
 )
 def test_plot_with_custom_config(
-    set_agg_backend,
-    close_all_figs_after_test,
-    binary_data_dispatcher: Tuple[
-        Dict[Hashable, bool], Dict[Hashable, bool], Dict[Hashable, float]
-    ],
+    set_agg_backend, close_all_figs_after_test, binary_data_dispatcher: BinaryDataTuple
 ):
     id_to_is_pos, id_to_pred_pos, _ = binary_data_dispatcher
     config = ConfusionMatrixPlotConfig(
